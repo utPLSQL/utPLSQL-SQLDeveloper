2808b97314b083e81f7ae7729c640029c17324d2
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-25 00:02:16 +0200

    convert OutputLines to Java

--- sqldev/src/main/java/org/utplsql/sqldev/model/ut/OutputLines.java
+++ sqldev/src/main/java/org/utplsql/sqldev/model/ut/OutputLines.java
@@ -13,13 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.model.ut
+package org.utplsql.sqldev.model.ut;
 
-import org.eclipse.xtend.lib.annotations.Accessors
-import org.utplsql.sqldev.model.AbstractModel
+import org.springframework.core.style.ToStringCreator;
+import org.utplsql.sqldev.model.AbstractModel;
 
-@Accessors
-class OutputLines extends AbstractModel {
-	String[] lines;
-	Integer numlines;
+public class OutputLines extends AbstractModel {
+    private String[] lines;
+    private Integer numlines;
+
+    @Override
+    public String toString() {
+        return new ToStringCreator(this, getStyler())
+                .append("lines", lines)
+                .append("numlines", numlines)
+                .toString();
+    }
+
+    public String[] getLines() {
+        return this.lines;
+    }
+
+    public void setLines(final String[] lines) {
+        this.lines = lines;
+    }
+
+    public Integer getNumlines() {
+        return this.numlines;
+    }
+
+    public void setNumlines(final Integer numlines) {
+        this.numlines = numlines;
+    }
 }
