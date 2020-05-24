53894c2474c60b2977ccc51714f3eb538dc0bd60
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-24 16:54:39 +0200

    convert XMLTools to Java

--- sqldev/src/main/java/org/utplsql/sqldev/model/XMLTools.java
+++ sqldev/src/main/java/org/utplsql/sqldev/model/XMLTools.java
@@ -1,4 +1,5 @@
-/* Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
+/*
+ * Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,77 +13,120 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.model
+package org.utplsql.sqldev.model;
 
-import java.io.StringWriter
-import javax.xml.transform.OutputKeys
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.dom.DOMSource
-import javax.xml.transform.stream.StreamResult
-import javax.xml.xpath.XPathConstants
-import javax.xml.xpath.XPathFactory
-import org.w3c.dom.Element
-import org.w3c.dom.Node
-import org.w3c.dom.NodeList
+import java.io.StringWriter;
+import java.util.logging.Logger;
 
-class XMLTools {
-	val xpathFactory = XPathFactory.newInstance()
-	val xpath = xpathFactory.newXPath()
-		
-	def getNodeList(Node doc, String xpathString) {
-		val expr = xpath.compile(xpathString);
-		val NodeList nodeList = expr.evaluate(doc, XPathConstants.NODESET) as NodeList
-		return nodeList 
-	}
-	
-	def getNode(Node doc, String xpathString) {
-		val expr = xpath.compile(xpathString);
-		val Node node = expr.evaluate(doc, XPathConstants.NODE) as Node
-		return node 
-	}
-	
-	def void trimWhitespace(Node node) {
-		val children = node.childNodes
-		for (i : 0 ..< children.length) {
-			val child = children.item(i)
-			if (child.nodeType == Node.TEXT_NODE) {
-				child.textContent = child.textContent.trim
-			}
-			trimWhitespace(child);
-		}
-	}
-	
-	def nodeToString(Node node, String cdataSectionElements) {
-		node.trimWhitespace
-		val writer = new StringWriter()
-		val factory = TransformerFactory.newInstance().newTransformer()
-		factory.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
-		factory.setOutputProperty(OutputKeys.INDENT, "yes")
-		factory.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
-		factory.setOutputProperty(OutputKeys.CDATA_SECTION_ELEMENTS, cdataSectionElements)
-		factory.transform(new DOMSource(node), new StreamResult(writer))
-		val result = writer.toString()
-		val fixedResult = result.replaceAll('''<!\[CDATA\[\s*\]\]>''',"")
-		return fixedResult
-	}
-	
-	def getAttributeValue(Node node, String namedItem) {
-		var String value = null
-		if (node instanceof Element) {
-			value = node.attributes?.getNamedItem(namedItem)?.nodeValue;
-		}
-		return value
-	}
-	
-	def getElementValue(Node node, String tagName) {
-		return getElementNode(node, tagName)?.textContent
-	}
+import javax.xml.XMLConstants;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
-	def getElementNode(Node node, String tagName) {
-		var Node resultNode = null
-		if (node instanceof Element) {
-			resultNode = node.getElementsByTagName(tagName)?.item(0)
-		}
-		return resultNode
-	}
+import org.utplsql.sqldev.exception.GenericRuntimeException;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLTools {
+    private static final Logger logger = Logger.getLogger(XMLTools.class.getName());
+    private final XPathFactory xpathFactory = XPathFactory.newInstance();
+    private final XPath xpath = xpathFactory.newXPath();
+
+    public NodeList getNodeList(final Node doc, final String xpathString) {
+        try {
+            final XPathExpression expr = xpath.compile(xpathString);
+            return ((NodeList) expr.evaluate(doc, XPathConstants.NODESET));
+        } catch (XPathExpressionException e) {
+            final String msg = "XPathExpressionException for " + xpathString + " due to " + e.getMessage();
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
+        }
+    }
+
+    public Node getNode(final Node doc, final String xpathString) {
+        try {
+            final XPathExpression expr = xpath.compile(xpathString);
+            return ((Node) expr.evaluate(doc, XPathConstants.NODE));
+        } catch (XPathExpressionException e) {
+            final String msg = "XPathExpressionException for " + xpathString + " due to " + e.getMessage();
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
+        }
+    }
+
+    public void trimWhitespace(final Node node) {
+        final NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            final Node child = children.item(i);
+            if (child.getNodeType() == Node.TEXT_NODE) {
+                child.setTextContent(child.getTextContent().trim());
+            }
+            trimWhitespace(child);
+        }
+    }
+
+    public String nodeToString(final Node node, final String cdataSectionElements) {
+        try {
+            trimWhitespace(node);
+            final StringWriter writer = new StringWriter();
+            TransformerFactory factory = TransformerFactory.newInstance();
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            final Transformer transformer = factory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
+            transformer.setOutputProperty(OutputKeys.CDATA_SECTION_ELEMENTS, cdataSectionElements);
+            transformer.transform( new DOMSource(node), new StreamResult(writer));
+            final String result = writer.toString();
+            return result.replaceAll("<!\\[CDATA\\[\\s*\\]\\]>", "");
+        } catch (TransformerException  e) {
+            final String msg = "TransformerException for " + cdataSectionElements + " due to " + e.getMessage();
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
+        }
+    }
+
+    public String getAttributeValue(final Node node, final String namedItem) {
+        String value = null;
+        if (node instanceof Element) {
+            final NamedNodeMap attributes = ((Element) node).getAttributes();
+            if (attributes != null) {
+                final Node item = attributes.getNamedItem(namedItem);
+                if (item != null) {
+                    value = item.getNodeValue();
+                }
+            }
+        }
+        return value;
+    }
+
+    public String getElementValue(final Node node, final String tagName) {
+        String value = null;
+        final Node item = getElementNode(node, tagName);
+        if (item != null) {
+            value = item.getTextContent();
+        }
+        return value;
+    }
+
+    public Node getElementNode(final Node node, final String tagName) {
+        Node resultNode = null;
+        if (node instanceof Element) {
+            NodeList list = ((Element) node).getElementsByTagName(tagName);
+            if (list != null && list.getLength() > 0) {
+                resultNode = list.item(0);
+            }
+        }
+        return resultNode;
+    }
 }
