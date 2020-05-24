81bf82193c43443fe816d14189a41f20fb9388a7
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-24 23:52:30 +0200

    moved STYLER instance to UtplsqlToStringStyler

--- sqldev/src/main/java/org/utplsql/sqldev/model/UtplsqlToStringStyler.java
+++ sqldev/src/main/java/org/utplsql/sqldev/model/UtplsqlToStringStyler.java
@@ -16,9 +16,11 @@
 package org.utplsql.sqldev.model;
 
 import org.springframework.core.style.DefaultToStringStyler;
+import org.springframework.core.style.ToStringStyler;
 import org.springframework.util.ClassUtils;
 
 public class UtplsqlToStringStyler extends DefaultToStringStyler {
+    public static final ToStringStyler STYLER = new UtplsqlToStringStyler();
 
     public UtplsqlToStringStyler() {
         super(new UtplsqlValueStyler());
