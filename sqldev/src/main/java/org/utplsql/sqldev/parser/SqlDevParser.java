4bf6d20752f6ef410aaf4a26f574fb75db683b38
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-25 18:45:42 +0200

    convert SqlDevParser to Java

--- sqldev/src/main/java/org/utplsql/sqldev/parser/SqlDevParser.java
+++ sqldev/src/main/java/org/utplsql/sqldev/parser/SqlDevParser.java
@@ -1,4 +1,5 @@
-/* Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
+/*
+ * Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,49 +13,56 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.parser
+package org.utplsql.sqldev.parser;
 
-import oracle.dbtools.parser.LexerToken
-import oracle.dbtools.raptor.navigator.plsql.PlSqlArguments
-import oracle.dbtools.raptor.navigator.plsql.PlsqlStructureParser
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import oracle.dbtools.parser.LexerToken;
+import oracle.dbtools.raptor.navigator.plsql.Member;
+import oracle.dbtools.raptor.navigator.plsql.PlSqlArguments;
+import oracle.dbtools.raptor.navigator.plsql.PlsqlStructureParser;
 
 /*
  * Cannot use this class within SQL Developer because the
  * package oracle.dbtools.parser is not exported in sqldeveloper OSGI bundle (extension)
  * (throws ClassNotFoundException at runtime).
  * 
- * The dbtools-common.jar contains the necessary packages, 
- * but it cannot be distributed with the utPLSQL extension 
+ * The dbtools-common.jar contains the necessary packages,
+ * but it cannot be distributed with the utPLSQL extension
  * without violating the Oracle license agreement.
  */
-class SqlDevParser {
-	def getMembers(String plsql) {
-		val tokens = LexerToken.parse(plsql)
-		val parser = new PlsqlStructureParser
-		parser.parse(tokens, PlSqlArguments.sort)
-		return parser.children
-	}
-	
-	private def getStartLine(String plsql, int offset) {
-		var int line = 1
-		for (var i = 0; i < plsql.length; i++) {
-			val c = plsql.substring(i, i+1)
-			if (i > offset) {
-				return line
-			} else if (c == '\n') {
-				line = line + 1
-			}
-		}
-		return line
-	}
-	
-	def getMemberStartLine(String plsql, String memberName) {
-		val members = plsql.members
-		val member = members.findFirst[it.name.equalsIgnoreCase(memberName)]
-		if (member !== null) {
-			return getStartLine(plsql, member.codeOffset)
-		} else {
-			1
-		}
-	}
+public class SqlDevParser {
+
+    @SuppressWarnings("unchecked")
+    public Set<Member> getMembers(final String plsql) {
+        final List<LexerToken> tokens = LexerToken.parse(plsql);
+        final PlsqlStructureParser parser = new PlsqlStructureParser();
+        parser.parse(tokens, PlSqlArguments.getSort());
+        return parser.children;
+    }
+
+    private int getStartLine(final String plsql, final int offset) {
+        int line = 1;
+        for (int i = 0; i < plsql.length(); i++) {
+            final String c = plsql.substring(i, i + 1);
+            if (i > offset) {
+                return line;
+            } else if ("\n".equals(c)) {
+                line = line + 1;
+            }
+        }
+        return line;
+    }
+
+    public int getMemberStartLine(final String plsql, final String memberName) {
+        final Set<Member> members = this.getMembers(plsql);
+        final Optional<Member> member = members.stream().filter(it -> it.name.equalsIgnoreCase(memberName)).findFirst();
+        if (member.isPresent()) {
+            return this.getStartLine(plsql, member.get().codeOffset);
+        } else {
+            return 1;
+        }
+    }
 }
