1cf53d54fadfbdfd79b6a7b449d0cd0db98a7fef
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-25 09:17:33 +0200

    convert Expectation to Java

--- sqldev/src/main/java/org/utplsql/sqldev/model/runner/Expectation.java
+++ sqldev/src/main/java/org/utplsql/sqldev/model/runner/Expectation.java
@@ -13,37 +13,89 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.model.runner
+package org.utplsql.sqldev.model.runner;
 
-import java.util.regex.Pattern
-import org.eclipse.xtend.lib.annotations.Accessors
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@Accessors
-class Expectation {
-	String description
-	String message
-	String caller
-	
-	def getFailureText() {
-		return '''
-			«message.trim»
-			«caller?.trim»
-		'''.toString.trim
-	}
-	
-	def getShortFailureText() {
-		return '''«IF description !== null»«description» (line «callerLine»)«ELSE»Line «callerLine»«ENDIF»'''.toString
-	}
-	
-	def getCallerLine() {
-		var Integer line = null
-		if (caller !== null) {
-			val p = Pattern.compile("(?i)\"[^\\\"]+\",\\s+line\\s*([0-9]+)")
-			val m = p.matcher(caller)
-			if (m.find) {
-				line = Integer.valueOf(m.group(1))
-			}
-		}
-		return line
-	}
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.springframework.core.style.ToStringCreator;
+import org.utplsql.sqldev.model.UtplsqlToStringStyler;
+
+public class Expectation {
+    private String description;
+    private String message;
+    private String caller;
+
+    @Override
+    public String toString() {
+        return new ToStringCreator(this, UtplsqlToStringStyler.INSTANCE)
+                .append("description", description)
+                .append("message", message)
+                .append("caller", caller)
+                .append("failureText", getFailureText())
+                .append("shortFailureText", getShortFailureText())
+                .append("callerLine", getCallerLine())
+                .toString();
+    }
+
+    public String getFailureText() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(message.trim());
+        if (caller != null) {
+            sb.append('\n');
+            sb.append(caller.trim());
+        }
+        return sb.toString();
+    }
+
+    public String getShortFailureText() {
+        final StringConcatenation sb = new StringConcatenation();
+        if (description != null) {
+            sb.append(description);
+            sb.append(" (line ");
+            sb.append(getCallerLine());
+            sb.append(")");
+        } else {
+            sb.append("Line ");
+            sb.append(getCallerLine());
+        }
+        return sb.toString();
+    }
+
+    public Integer getCallerLine() {
+        Integer line = null;
+        if (caller != null) {
+            final Pattern p = Pattern.compile("(?i)\"[^\\\"]+\",\\s+line\\s*([0-9]+)");
+            final Matcher m = p.matcher(caller);
+            if (m.find()) {
+                line = Integer.valueOf(m.group(1));
+            }
+        }
+        return line;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getCaller() {
+        return caller;
+    }
+
+    public void setCaller(final String caller) {
+        this.caller = caller;
+    }
 }
