1689fa1c3e91c8af6977f50b70c0ea13d3365933
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-25 13:53:29 +0200

    convert RunGenerator to Java

--- sqldev/src/main/java/org/utplsql/sqldev/oddgen/RunGenerator.java
+++ sqldev/src/main/java/org/utplsql/sqldev/oddgen/RunGenerator.java
@@ -13,167 +13,196 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.oddgen
+package org.utplsql.sqldev.oddgen;
 
-import java.sql.Connection
-import java.util.ArrayList
-import java.util.HashMap
-import java.util.HashSet
-import java.util.LinkedHashMap
-import java.util.List
-import oracle.ide.config.Preferences
-import org.oddgen.sqldev.generators.OddgenGenerator2
-import org.oddgen.sqldev.generators.model.Node
-import org.utplsql.sqldev.dal.UtplsqlDao
-import org.utplsql.sqldev.model.preference.PreferenceModel
-import org.utplsql.sqldev.resources.UtplsqlResources
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-class RunGenerator implements OddgenGenerator2 {
+import org.oddgen.sqldev.generators.OddgenGenerator2;
+import org.oddgen.sqldev.generators.model.Node;
+import org.utplsql.sqldev.dal.UtplsqlDao;
+import org.utplsql.sqldev.exception.GenericDatabaseAccessException;
+import org.utplsql.sqldev.model.StringTools;
+import org.utplsql.sqldev.model.preference.PreferenceModel;
+import org.utplsql.sqldev.resources.UtplsqlResources;
 
-	public static val YES = "Yes"
-	public static val NO = "No"
-	
-	public static var RESET_PACKAGE = UtplsqlResources.getString("PREF_RESET_PACKAGE_LABEL")
-	public static var CLEAR_SCREEN = UtplsqlResources.getString("PREF_CLEAR_SCREEN_LABEL")
-	public static var INDENT_SPACES = UtplsqlResources.getString("PREF_INDENT_SPACES_LABEL")
-	
-	// oddgen node cache
-	var List<Node> runnables = null;
+import oracle.ide.config.Preferences;
 
-	override isSupported(Connection conn) {
-		var ret = false
-		if (conn !== null) {
-			if (conn.metaData.databaseProductName.startsWith("Oracle")) {
-				if (conn.metaData.databaseMajorVersion == 11) {
-					if (conn.metaData.databaseMinorVersion >= 2) {
-						ret = true
-					}
-				} else if (conn.metaData.databaseMajorVersion > 11) {
-					ret = true
-				}
-			}
-		}
-		return ret
-	}
+public class RunGenerator implements OddgenGenerator2 {
+    private static final Logger logger = Logger.getLogger(RunGenerator.class.getName());
 
-	override getName(Connection conn) {
-		return "Run test"
-	}
+    public static final String YES = "Yes";
+    public static final String NO = "No";
+    public static final String RESET_PACKAGE = UtplsqlResources.getString("PREF_RESET_PACKAGE_LABEL");
+    public static final String CLEAR_SCREEN = UtplsqlResources.getString("PREF_CLEAR_SCREEN_LABEL");
+    public static final String INDENT_SPACES = UtplsqlResources.getString("PREF_INDENT_SPACES_LABEL");
 
-	override getDescription(Connection conn) {
-		return "Runs utPLSQL test packages in the current user."
-	}
+    // oddgen node cache
+    private List<Node> runnables = null;
 
-	override getFolders(Connection conn) {
-		val preferences = PreferenceModel.getInstance(Preferences.preferences)
-		val folders = new ArrayList<String>
-		for (f : preferences.rootFolderInOddgenView.split(",").filter[!it.empty]) {
-			folders.add(f.trim)
-		}
-		return folders
-	}
+    @Override
+    public boolean isSupported(final Connection conn) {
+        try {
+            boolean ret = false;
+            if (conn != null && conn.getMetaData().getDatabaseProductName().startsWith("Oracle")
+                    && (conn.getMetaData().getDatabaseMajorVersion() == 11
+                            && conn.getMetaData().getDatabaseMinorVersion() >= 2
+                            || conn.getMetaData().getDatabaseMajorVersion() > 11)) {
+                ret = true;
+            }
+            return ret;
+        } catch (SQLException e) {
+            final String msg = "SQLException during connection check due to " + e.getMessage();
+            logger.severe(() -> msg);
+            throw new GenericDatabaseAccessException(msg, e);
+        }
+    }
 
-	override getHelp(Connection conn) {
-		return "<p>not yet available</p>"
-	}
-	
-	override getNodes(Connection conn, String parentNodeId) {
-		// oddgen asks for children for each parent node, regardless of load strategy (eager/lazy)
-		// oddgen does not know about the load strategy, hence caching is the responsibility of the generator
-		if (runnables === null) {
-			val preferences = PreferenceModel.getInstance(Preferences.preferences)
-			val params = new LinkedHashMap<String, String>()
-			params.put(RESET_PACKAGE, if (preferences.resetPackage) {YES} else {NO})
-			params.put(CLEAR_SCREEN, if (preferences.clearScreen) {YES} else {NO})
-			params.put(INDENT_SPACES, String.valueOf(preferences.indentSpaces))
-			val UtplsqlDao dao = new UtplsqlDao(conn)
-			// load node tree eagerly (all nodes in one go)
-			runnables = dao.runnables
-			for (node : runnables) {
-				node.params = params
-			}
-		}
-		return runnables
-	}
+    @Override
+    public String getName(final Connection conn) {
+        return "Run test";
+    }
 
-	override getLov(Connection conn, LinkedHashMap<String, String> params, List<Node> nodes) {
-		val lov = new HashMap<String, List<String>>()
-		lov.put(RESET_PACKAGE, #[YES, NO])
-		lov.put(CLEAR_SCREEN, #[YES, NO])
-		lov.put(INDENT_SPACES, #["1", "2", "3", "4", "5", "6", "7", "8"])
-		return lov
-	}
+    @Override
+    public String getDescription(final Connection conn) {
+        return "Runs utPLSQL test packages in the current user.";
+    }
 
-	override getParamStates(Connection conn, LinkedHashMap<String, String> params, List<Node> nodes) {
-		return new HashMap<String, Boolean>
-	}
-	
-	private def getPath(Node node, Connection conn) {
-		if (node.id == "SUITE" || node.id == "SUITEPATH") {
-			return conn.metaData.userName
-		} else {
-			return node.id
-		}
-	}
+    @Override
+    public List<String> getFolders(final Connection conn) {
+        final PreferenceModel preferences = PreferenceModel.getInstance(Preferences.getPreferences());
+        final ArrayList<String> folders = new ArrayList<>();
+        for (String f : preferences.getRootFolderInOddgenView().split(",")) {
+            if (f != null) {
+                folders.add(f.trim());
+            }
+        }
+        return folders;
+    }
 
-	private def replaceTabsWithSpaces(CharSequence input, int indentSpaces) {
-		val spaces = String.format("%1$"+indentSpaces+"s", "")
-		return input.toString.replace("\t", spaces)
-	}
-	
-	def dedup(List<Node> nodes) {
-		val set = new HashSet<String>
-		for (node : nodes) {
-			set.add(node.id)
-		}
-		val ret = new ArrayList<Node>
-		for (node : nodes) {
-			if (!set.contains(node.parentId)) {
-				ret.add(node)
-			}
-		}
-		return ret
-	}
-	
-	override generateProlog(Connection conn, List<Node> nodes) {
-		val dedupNodes = nodes.dedup
-		val params = dedupNodes.get(0).params
-		val ret = '''
-			«IF params.get(RESET_PACKAGE) == YES»
-				EXECUTE dbms_session.reset_package;
-			«ENDIF»
-			SET SERVEROUTPUT ON SIZE UNLIMITED
-			«IF params.get(CLEAR_SCREEN) == YES»
-				CLEAR SCREEN
-			«ENDIF»
-			«IF dedupNodes.size == 1»
-				EXECUTE ut.run('«dedupNodes.get(0).getPath(conn)»');
-			«ELSE»
-				BEGIN
-					ut.run(
-						ut_varchar2_list(
-							«FOR node : dedupNodes SEPARATOR ","»
-								'«node.getPath(conn)»'
-							«ENDFOR»
-						)
-					);
-				END;
-				/
-			«ENDIF»
-		'''
-		return ret.replaceTabsWithSpaces(Integer.valueOf(params.get(INDENT_SPACES)))
-	}
+    @Override
+    public String getHelp(final Connection conn) {
+        return "<p>not yet available</p>";
+    }
 
-	override generateSeparator(Connection conn) {
-		return ""
-	}
-	
-	override generateEpilog(Connection conn, List<Node> nodes) {
-		return ""
-	}
-	
-	override generate(Connection conn, Node node) {
-		return ""
-	}
-	
+    @Override
+    public List<Node> getNodes(final Connection conn, final String parentNodeId) {
+        // oddgen asks for children for each parent node, regardless of load strategy (eager/lazy)
+        // oddgen does not know about the load strategy, hence caching is the responsibility of the generator
+        if (runnables == null) {
+            final PreferenceModel preferences = PreferenceModel.getInstance(Preferences.getPreferences());
+            final LinkedHashMap<String, String> params = new LinkedHashMap<>();
+            params.put(RESET_PACKAGE, preferences.isResetPackage() ? "YES" : "NO");
+            params.put(CLEAR_SCREEN, preferences.isClearScreen() ? "YES" : "NO");
+            params.put(INDENT_SPACES, String.valueOf(preferences.getIndentSpaces()));
+            final UtplsqlDao dao = new UtplsqlDao(conn);
+            // load node tree eagerly (all nodes in one go)
+            runnables = dao.runnables();
+            for (final Node node : runnables) {
+                node.setParams(params);
+            }
+        }
+        return runnables;
+    }
+
+    @Override
+    public HashMap<String, List<String>> getLov(final Connection conn, final LinkedHashMap<String, String> params, final List<Node> nodes) {
+        final HashMap<String, List<String>> lov = new HashMap<>();
+        lov.put(RESET_PACKAGE, Arrays.asList(YES, NO));
+        lov.put(CLEAR_SCREEN, Arrays.asList(YES, NO));
+        lov.put(INDENT_SPACES, Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8"));
+        return lov;
+    }
+
+    @Override
+    public HashMap<String, Boolean> getParamStates(final Connection conn, final LinkedHashMap<String, String> params, final List<Node> nodes) {
+        return new HashMap<>();
+    }
+
+    private String getPath(final Node node, final Connection conn) {
+        if ("SUITE".equals(node.getId()) || "SUITEPATH".equals(node.getId())) {
+            try {
+                return conn.getMetaData().getUserName();
+            } catch (SQLException e) {
+                final String msg = "SQLException during getUserName() due to " + e.getMessage();
+                logger.severe(() -> msg);
+                throw new GenericDatabaseAccessException(msg, e);
+            }
+        } else {
+            return node.getId();
+        }
+    }
+
+    private String replaceTabsWithSpaces(final CharSequence input, final int indentSpaces) {
+        final String spaces = String.format((("%1$" + Integer.valueOf(indentSpaces)) + "s"), "");
+        return input.toString().replace("\t", spaces);
+    }
+
+    public ArrayList<Node> dedup(final List<Node> nodes) {
+        final HashSet<String> set = new HashSet<>();
+        for (final Node node : nodes) {
+            set.add(node.getId());
+        }
+        final ArrayList<Node> ret = new ArrayList<>();
+        for (final Node node : nodes) {
+            if (!set.contains(node.getParentId())) {
+                ret.add(node);
+            }
+        }
+        return ret;
+    }
+
+    @Override
+    public String generateProlog(final Connection conn, final List<Node> nodes) {
+        final ArrayList<Node> dedupNodes = dedup(nodes);
+        final LinkedHashMap<String, String> params = dedupNodes.get(0).getParams();
+        final StringBuilder sb = new StringBuilder();
+        if ("YES".equals(params.get(RESET_PACKAGE))) {
+            sb.append("EXECUTE dbms_session.reset_package;\n");
+        }
+        sb.append("SET SERVEROUTPUT ON SIZE UNLIMITED\n");
+        if ("YES".equals(params.get(CLEAR_SCREEN))) {
+            sb.append("CLEAR SCREEN\n");
+        }
+        if (dedupNodes.size() == 1) {
+            sb.append("EXECUTE ut.run('");
+            sb.append(getPath(dedupNodes.get(0), conn));
+            sb.append("');\n");
+        } else {
+            final List<String> paths = nodes.stream().map(node -> getPath(node, conn)).collect(Collectors.toList());
+            sb.append("BEGIN\n");
+            sb.append("\tut.run(\n");
+            sb.append("\t\tut_varchar2_list(\n");
+            sb.append(StringTools.getCSV(paths, "\t\t\t"));
+            sb.append("\t\t)\n");
+            sb.append("\t);\n");
+            sb.append("END;\n");
+            sb.append("/\n");
+        }
+        final String ret = sb.toString();
+        return replaceTabsWithSpaces(ret, (Integer.valueOf(params.get(INDENT_SPACES))));
+    }
+
+    @Override
+    public String generateSeparator(final Connection conn) {
+        return "";
+    }
+
+    @Override
+    public String generateEpilog(final Connection conn, final List<Node> nodes) {
+        return "";
+    }
+
+    @Override
+    public String generate(final Connection conn, final Node node) {
+        return "";
+    }
 }
