3c8305201d1ab1bf8a810e79edc198693fca1308
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-26 21:28:54 +0200

    convert SnippetMerger to Java, removing Xtend dependencies

--- sqldev/src/main/java/org/utplsql/sqldev/snippet/SnippetMerger.java
+++ sqldev/src/main/java/org/utplsql/sqldev/snippet/SnippetMerger.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,115 +25,127 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
+
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import oracle.dbtools.util.Resource;
-import org.eclipse.xtend2.lib.StringConcatenation;
-import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.eclipse.xtext.xbase.lib.ExclusiveRange;
-import org.eclipse.xtext.xbase.lib.Extension;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.utplsql.sqldev.exception.GenericRuntimeException;
 import org.utplsql.sqldev.model.XMLTools;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
-@SuppressWarnings("all")
+import oracle.dbtools.util.Resource;
+
 public class SnippetMerger {
-  @Extension
-  private final XMLTools xmlTools = new XMLTools();
-  
-  private File userSnippetsFile;
-  
-  private String utplsqlSnippets;
-  
-  public String getUtplsqlSnippetsAsString() throws IOException {
-    final InputStream stream = this.getClass().getResourceAsStream("/org/utplsql/sqldev/resources/UtplsqlSnippets.xml");
-    Charset _defaultCharset = Charset.defaultCharset();
-    InputStreamReader _inputStreamReader = new InputStreamReader(stream, _defaultCharset);
-    final BufferedReader reader = new BufferedReader(_inputStreamReader);
-    return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-  }
-  
-  public SnippetMerger() {
-    this(new File(((Resource.RAPTOR_USER.getAbsolutePath() + File.separator) + "UserSnippets.xml")));
-  }
-  
-  public SnippetMerger(final File file) {
-    try {
-      this.utplsqlSnippets = this.getUtplsqlSnippetsAsString();
-      this.userSnippetsFile = file;
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
+    private static final Logger logger = Logger.getLogger(SnippetMerger.class.getName());
+
+    private final XMLTools xmlTools = new XMLTools();
+    private File userSnippetsFile;
+    private String utplsqlSnippets;
+
+    public String getUtplsqlSnippetsAsString() {
+        final InputStream stream = getClass()
+                .getResourceAsStream("/org/utplsql/sqldev/resources/UtplsqlSnippets.xml");
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(stream, Charset.defaultCharset()));
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
     }
-  }
-  
-  public Path merge() {
-    try {
-      Path _xblockexpression = null;
-      {
-        String result = null;
-        boolean _exists = this.userSnippetsFile.exists();
-        if (_exists) {
-          byte[] _readAllBytes = Files.readAllBytes(Paths.get(this.userSnippetsFile.getAbsolutePath()));
-          final String userSnippets = new String(_readAllBytes);
-          final DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-          StringReader _stringReader = new StringReader(userSnippets);
-          InputSource _inputSource = new InputSource(_stringReader);
-          final Document userSnippetsDoc = docBuilder.parse(_inputSource);
-          StringConcatenation _builder = new StringConcatenation();
-          _builder.append("/snippets/group[not(@category=\"utPLSQL Annotations\" or @category=\"utPLSQL Expectations\")]");
-          final NodeList userSnippetsGroups = this.xmlTools.getNodeList(userSnippetsDoc, _builder.toString());
-          StringReader _stringReader_1 = new StringReader(this.utplsqlSnippets);
-          InputSource _inputSource_1 = new InputSource(_stringReader_1);
-          final Document utplsqlSnippetsDoc = docBuilder.parse(_inputSource_1);
-          StringConcatenation _builder_1 = new StringConcatenation();
-          _builder_1.append("/snippets/group");
-          final NodeList utplsqlSnippetsGroups = this.xmlTools.getNodeList(utplsqlSnippetsDoc, _builder_1.toString());
-          StringConcatenation _builder_2 = new StringConcatenation();
-          _builder_2.append("<?xml version = \'1.0\' encoding = \'UTF-8\'?>");
-          _builder_2.newLine();
-          _builder_2.append("<snippets>");
-          _builder_2.newLine();
-          {
-            int _length = userSnippetsGroups.getLength();
-            ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, _length, true);
-            for(final Integer i : _doubleDotLessThan) {
-              _builder_2.append("   ");
-              String _nodeToString = this.xmlTools.nodeToString(userSnippetsGroups.item((i).intValue()), "code");
-              _builder_2.append(_nodeToString, "   ");
-              _builder_2.newLineIfNotEmpty();
-            }
-          }
-          {
-            int _length_1 = utplsqlSnippetsGroups.getLength();
-            ExclusiveRange _doubleDotLessThan_1 = new ExclusiveRange(0, _length_1, true);
-            for(final Integer i_1 : _doubleDotLessThan_1) {
-              _builder_2.append("   ");
-              String _nodeToString_1 = this.xmlTools.nodeToString(utplsqlSnippetsGroups.item((i_1).intValue()), "code");
-              _builder_2.append(_nodeToString_1, "   ");
-              _builder_2.newLineIfNotEmpty();
-            }
-          }
-          _builder_2.append("</snippets>");
-          _builder_2.newLine();
-          result = _builder_2.toString();
-        } else {
-          result = this.utplsqlSnippets;
+
+    public SnippetMerger() {
+        // works in SQL Developer only, otherwise a ExceptionInInitializerError is thrown
+        this(new File(Resource.RAPTOR_USER.getAbsolutePath() + File.separator + "UserSnippets.xml"));
+    }
+
+    public SnippetMerger(final File file) {
+        utplsqlSnippets = getUtplsqlSnippetsAsString();
+        userSnippetsFile = file;
+    }
+    
+    private byte[] readFile(Path path) {
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            final String msg = "Cannot read file " + path.toString() + " due to " + e.getMessage() + ".";
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
         }
-        _xblockexpression = Files.write(Paths.get(this.userSnippetsFile.getAbsolutePath()), result.getBytes());
-      }
-      return _xblockexpression;
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
     }
-  }
-  
-  public String getTemplate() {
-    return this.utplsqlSnippets;
-  }
-  
-  public File getFile() {
-    return this.userSnippetsFile;
-  }
+    
+    private void writeFile(Path path, byte[] bytes) {
+        try {
+            Files.write(path, bytes);
+        } catch (IOException e) {
+            final String msg = "Cannot write file " + path.toString() + " due to " + e.getMessage() + ".";
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
+        }
+    }
+    
+    private DocumentBuilder createDocumentBuilder() {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, Boolean.TRUE);
+            return factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            final String msg = "Could not create no document builder due to " + e.getMessage() + ".";
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
+        }
+    }
+    
+    private Document parse(final DocumentBuilder builder, final InputSource inputSource) {
+        try {
+            return builder.parse(inputSource);
+        } catch (SAXException | IOException e) {
+            final String msg = "Could not parse XML input due to " + e.getMessage() + ".";
+            logger.severe(() -> msg);
+            throw new GenericRuntimeException(msg, e);
+        }
+    }
+
+    public void merge() {
+        String result = null;
+        if (userSnippetsFile.exists()) {
+            // file exists, proper merge required
+            final String userSnippets = new String(readFile(Paths.get(userSnippetsFile.getAbsolutePath())));
+            final DocumentBuilder docBuilder = createDocumentBuilder();
+            final Document userSnippetsDoc = parse(docBuilder, new InputSource(new StringReader(userSnippets)));
+            final NodeList userSnippetsGroups = xmlTools.getNodeList(userSnippetsDoc,
+                    "/snippets/group[not(@category=\"utPLSQL Annotations\" or @category=\"utPLSQL Expectations\")]");
+            final Document utplsqlSnippetsDoc = parse(docBuilder, new InputSource(new StringReader(utplsqlSnippets)));
+            final NodeList utplsqlSnippetsGroups = xmlTools.getNodeList(utplsqlSnippetsDoc, "/snippets/group");
+            StringBuilder sb = new StringBuilder();
+            sb.append("<?xml version = '1.0' encoding = 'UTF-8'?>\n");
+            sb.append("<snippets>\n");
+            for (int i = 0; i < userSnippetsGroups.getLength(); i++) {
+                sb.append("   ");
+                sb.append(xmlTools.nodeToString(userSnippetsGroups.item(i), "code"));
+                sb.append('\n');
+            }
+            for (int i = 0; i < utplsqlSnippetsGroups.getLength(); i ++) {
+                sb.append("   ");
+                sb.append(xmlTools.nodeToString(utplsqlSnippetsGroups.item(i), "code"));
+                sb.append('\n');
+            }
+            sb.append("</snippets>\n");
+            result = sb.toString();
+        } else {
+            // just copy
+            result = utplsqlSnippets;
+        }
+        writeFile(Paths.get(userSnippetsFile.getAbsolutePath()), result.getBytes());
+    }
+
+    public String getTemplate() {
+        return utplsqlSnippets;
+    }
+
+    public File getFile() {
+        return userSnippetsFile;
+    }
 }
