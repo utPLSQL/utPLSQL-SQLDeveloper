b76064479acae1a0ceb0222c6f0662c8a27313f0
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-27 09:52:47 +0200

    convert CodeCoverageReporterDialog to Java, removing Xtend dependencies

--- sqldev/src/main/java/org/utplsql/sqldev/ui/coverage/CodeCoverageReporterDialog.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/coverage/CodeCoverageReporterDialog.java
@@ -15,7 +15,6 @@
  */
 package org.utplsql.sqldev.ui.coverage;
 
-import com.google.common.base.Objects;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -30,256 +29,210 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.WindowEvent;
 import java.sql.Connection;
-import java.util.List;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.JViewport;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
-import org.eclipse.xtend2.lib.StringConcatenation;
-import org.eclipse.xtext.xbase.lib.Exceptions;
+
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.utplsql.sqldev.coverage.CodeCoverageReporter;
+import org.utplsql.sqldev.exception.GenericDatabaseAccessException;
+import org.utplsql.sqldev.model.StringTools;
 import org.utplsql.sqldev.resources.UtplsqlResources;
 
-@SuppressWarnings("all")
 public class CodeCoverageReporterDialog extends JFrame implements ActionListener, FocusListener {
-  private CodeCoverageReporter reporter;
-  
-  private JButton runButton;
-  
-  private JButton cancelButton;
-  
-  private JPanel paneParams;
-  
-  private int paramPos = (-1);
-  
-  private final JTextArea pathsTextArea = new JTextArea();
-  
-  private final JTextField schemasTextField = new JTextField();
-  
-  private final JTextArea includeObjectsTextArea = new JTextArea();
-  
-  private final JTextArea excludeObjectsTextArea = new JTextArea();
-  
-  public static void createAndShow(final CodeCoverageReporter reporter) {
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        CodeCoverageReporterDialog.createAndShowWithinEventThread(reporter);
-      }
-    });
-  }
-  
-  private static void createAndShowWithinEventThread(final CodeCoverageReporter reporter) {
-    final CodeCoverageReporterDialog frame = new CodeCoverageReporterDialog(reporter);
-    reporter.setFrame(frame);
-    frame.pack();
-    final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-    frame.setLocation(((dim.width / 2) - (frame.getSize().width / 2)), ((dim.height / 2) - (frame.getSize().height / 2)));
-    frame.setAlwaysOnTop(true);
-    frame.setVisible(true);
-  }
-  
-  public CodeCoverageReporterDialog(final CodeCoverageReporter reporter) {
-    super(UtplsqlResources.getString("WINDOW_CODE_COVERAGE_REPORT_LABEL"));
-    this.reporter = reporter;
-    final Container pane = this.getContentPane();
-    GridBagLayout _gridBagLayout = new GridBagLayout();
-    pane.setLayout(_gridBagLayout);
-    final GridBagConstraints c = new GridBagConstraints();
-    GridBagLayout _gridBagLayout_1 = new GridBagLayout();
-    JPanel _jPanel = new JPanel(_gridBagLayout_1);
-    this.paneParams = _jPanel;
-    this.pathsTextArea.setEditable(false);
-    this.pathsTextArea.setEnabled(false);
-    String _string = UtplsqlResources.getString("WINDOW_PATHS_LABEL");
-    StringConcatenation _builder = new StringConcatenation();
-    {
-      List<String> _pathList = reporter.getPathList();
-      boolean _hasElements = false;
-      for(final String path : _pathList) {
-        if (!_hasElements) {
-          _hasElements = true;
+    private static final long serialVersionUID = 5503685225300993401L;
+    private static final Logger logger = Logger.getLogger(CodeCoverageReporterDialog.class.getName());
+
+    private CodeCoverageReporter reporter;
+    private JButton runButton;
+    private JButton cancelButton;
+    private JPanel paneParams;
+    private int paramPos = (-1);
+    private final JTextArea pathsTextArea = new JTextArea();
+    private final JTextField schemasTextField = new JTextField();
+    private final JTextArea includeObjectsTextArea = new JTextArea();
+    private final JTextArea excludeObjectsTextArea = new JTextArea();
+
+    public static void createAndShow(final CodeCoverageReporter reporter) {
+        SwingUtilities.invokeLater(() -> CodeCoverageReporterDialog.createAndShowWithinEventThread(reporter));
+    }
+
+    private static void createAndShowWithinEventThread(final CodeCoverageReporter reporter) {
+        // create and layout the dialog
+        final CodeCoverageReporterDialog frame = new CodeCoverageReporterDialog(reporter);
+        reporter.setFrame(frame);
+        frame.pack();
+        // center dialog
+        final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+    }
+
+    public CodeCoverageReporterDialog(final CodeCoverageReporter reporter) {
+        super(UtplsqlResources.getString("WINDOW_CODE_COVERAGE_REPORT_LABEL"));
+        this.reporter = reporter;
+        final Container pane = getContentPane();
+        pane.setLayout(new GridBagLayout());
+        final GridBagConstraints c = new GridBagConstraints();
+        
+        // parameters pane
+        paneParams = new JPanel(new GridBagLayout());
+        pathsTextArea.setEditable(false);
+        pathsTextArea.setEnabled(false);
+        addParam(UtplsqlResources.getString("WINDOW_PATHS_LABEL"), StringTools.getSimpleCSV(reporter.getPathList()), pathsTextArea, 50, 2);
+        addParam(UtplsqlResources.getString("WINDOW_SCHEMAS_LABEL"), "", schemasTextField, 0, 0);
+        addParam(UtplsqlResources.getString("WINDOW_INCLUDE_OBJECS_LABEL"), StringTools.getSimpleCSV(reporter.getIncludeObjectList()), includeObjectsTextArea, 66, 4);
+        addParam(UtplsqlResources.getString("WINDOW_EXCLUDE_OBJECS_LABEL"), "", excludeObjectsTextArea, 34, 1);
+        final JScrollPane scrollPane = new JScrollPane(paneParams);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.insets = new Insets(10, 10, 0, 10); // top, left, bottom, right
+        c.anchor = GridBagConstraints.NORTH;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        pane.add(scrollPane, c);
+
+        // Buttons pane
+        final JPanel panelButtons = new JPanel(new GridBagLayout());
+        runButton = new JButton(UtplsqlResources.getString("WINDOW_RUN_BUTTON"));
+        runButton.addActionListener(this);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.insets = new Insets(0, 0, 0, 0); // top, left, bottom, right
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        c.weighty = 0;
+        panelButtons.add(runButton, c);
+        cancelButton = new JButton(UtplsqlResources.getString("WINDOW_CANCEL_BUTTON"));
+        cancelButton.addActionListener(this);
+        c.gridx = 1;
+        c.insets = new Insets(0, 10, 0, 0); // top, left, bottom, right
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        c.weighty = 0;
+        panelButtons.add(cancelButton, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.insets = new Insets(30, 10, 10, 10); // top, left, bottom, right
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        c.weighty = 0;
+        pane.add(panelButtons, c);
+        pane.setPreferredSize(new Dimension(500, 320));
+        SwingUtilities.getRootPane(runButton).setDefaultButton(runButton);
+    }
+
+    private void addParam(final String label, final String text, final Component component, final int height,
+            final double weighty) {
+        paramPos++;
+        final GridBagConstraints c = new GridBagConstraints();
+        final JLabel paramLabel = new JLabel(label);
+        c.gridx = 0;
+        c.gridy = paramPos;
+        c.gridwidth = 1;
+        c.insets =  new Insets(10, 10, 0, 0); // top, left, bottom, right
+        c.anchor = GridBagConstraints.NORTHWEST;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0;
+        c.weighty = 0;
+        paneParams.add(paramLabel, c);
+        c.gridx = 1;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(10, 10, 0, 10); // top, left, bottom, right
+        c.weightx = 1;
+        c.weighty = weighty;
+        if (component instanceof JTextField) {
+            ((JTextField) component).setText(text);
+            paneParams.add(component, c);
+        } else if (component instanceof JTextArea) {
+            ((JTextArea) component).setText(text);
+            ((JTextArea) component).setLineWrap(true);
+            ((JTextArea) component).setWrapStyleWord(true);
+            JScrollPane scrollPane = new JScrollPane(component);
+            scrollPane.getViewport().setPreferredSize(new Dimension(200, height));
+            paneParams.add(scrollPane, c);
+        }
+        component.addFocusListener(this);
+    }
+
+    public void exit() {
+        dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    }
+    
+    private void abortConnection(final Connection conn, final SimpleAsyncTaskExecutor taskExecutor) {
+        try {
+            conn.abort(taskExecutor);
+        } catch (SQLException e) {
+            final String msg = "Error aborting connection due to " + e.getMessage();
+            logger.severe(() -> msg);
+            throw new GenericDatabaseAccessException(msg, e);
+        }
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        if (e.getSource() == runButton) {
+            reporter.setSchemas(schemasTextField.getText());
+            reporter.setIncludeObjects(includeObjectsTextArea.getText());
+            reporter.setExcludeObjects(excludeObjectsTextArea.getText());
+            schemasTextField.setEnabled(false);
+            includeObjectsTextArea.setEnabled(false);
+            excludeObjectsTextArea.setEnabled(false);
+            runButton.setEnabled(false);
+            reporter.runAsync();
         } else {
-          _builder.appendImmediate(", ", "");
+            if (e.getSource() == cancelButton) {
+                if (runButton.isEnabled()) {
+                    // report is not yet started, just close the window
+                    exit();
+                } else {
+                    // report is being created...
+                    // frame will close as soon as the connection is technically aborted
+                    // database session is not cancelled. This is not a bug.
+                    // to cancel the session you have to kill it via "ALTER SYSTEM KILL SESSION".
+                    // However, the abort frees all resources on the client side. 
+                    abortConnection(reporter.getConnection(), new SimpleAsyncTaskExecutor());
+                }
+            }
         }
-        _builder.append(path);
-      }
     }
-    this.addParam(_string, _builder.toString(), this.pathsTextArea, 50, 2);
-    this.addParam(UtplsqlResources.getString("WINDOW_SCHEMAS_LABEL"), "", this.schemasTextField, 0, 0);
-    String _string_1 = UtplsqlResources.getString("WINDOW_INCLUDE_OBJECS_LABEL");
-    StringConcatenation _builder_1 = new StringConcatenation();
-    {
-      List<String> _includeObjectList = reporter.getIncludeObjectList();
-      boolean _hasElements_1 = false;
-      for(final String i : _includeObjectList) {
-        if (!_hasElements_1) {
-          _hasElements_1 = true;
-        } else {
-          _builder_1.appendImmediate(", ", "");
+
+    @Override
+    public void focusGained(final FocusEvent e) {
+        if (paneParams.isAncestorOf(e.getComponent())) {
+            // make component at cursor position is visible
+            final int x = e.getComponent().getLocationOnScreen().x - paneParams.getLocationOnScreen().x;
+            final int y = e.getComponent().getLocationOnScreen().y - paneParams.getLocationOnScreen().y;
+            final int width = e.getComponent().getBounds().width;
+            final int height = e.getComponent().getBounds().height;
+            final Rectangle rect = new Rectangle(x, y, width, height);
+            paneParams.scrollRectToVisible(rect);
         }
-        _builder_1.append(i);
-      }
     }
-    this.addParam(_string_1, _builder_1.toString(), this.includeObjectsTextArea, 66, 4);
-    this.addParam(UtplsqlResources.getString("WINDOW_EXCLUDE_OBJECS_LABEL"), "", this.excludeObjectsTextArea, 34, 1);
-    final JScrollPane scrollPane = new JScrollPane(this.paneParams);
-    scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-    scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-    scrollPane.setBorder(BorderFactory.createEmptyBorder());
-    c.gridx = 0;
-    c.gridy = 0;
-    c.gridwidth = 2;
-    Insets _insets = new Insets(10, 10, 0, 10);
-    c.insets = _insets;
-    c.anchor = GridBagConstraints.NORTH;
-    c.fill = GridBagConstraints.BOTH;
-    c.weightx = 1;
-    c.weighty = 1;
-    pane.add(scrollPane, c);
-    GridBagLayout _gridBagLayout_2 = new GridBagLayout();
-    final JPanel panelButtons = new JPanel(_gridBagLayout_2);
-    String _string_2 = UtplsqlResources.getString("WINDOW_RUN_BUTTON");
-    JButton _jButton = new JButton(_string_2);
-    this.runButton = _jButton;
-    this.runButton.addActionListener(this);
-    c.gridx = 0;
-    c.gridy = 0;
-    c.gridwidth = 1;
-    Insets _insets_1 = new Insets(0, 0, 0, 0);
-    c.insets = _insets_1;
-    c.fill = GridBagConstraints.NONE;
-    c.weightx = 0;
-    c.weighty = 0;
-    panelButtons.add(this.runButton, c);
-    String _string_3 = UtplsqlResources.getString("WINDOW_CANCEL_BUTTON");
-    JButton _jButton_1 = new JButton(_string_3);
-    this.cancelButton = _jButton_1;
-    this.cancelButton.addActionListener(this);
-    c.gridx = 1;
-    Insets _insets_2 = new Insets(0, 10, 0, 0);
-    c.insets = _insets_2;
-    c.fill = GridBagConstraints.NONE;
-    c.weightx = 0;
-    c.weighty = 0;
-    panelButtons.add(this.cancelButton, c);
-    c.gridx = 1;
-    c.gridy = 1;
-    c.gridwidth = 1;
-    Insets _insets_3 = new Insets(30, 10, 10, 10);
-    c.insets = _insets_3;
-    c.anchor = GridBagConstraints.EAST;
-    c.fill = GridBagConstraints.NONE;
-    c.weightx = 0;
-    c.weighty = 0;
-    pane.add(panelButtons, c);
-    Dimension _dimension = new Dimension(500, 320);
-    pane.setPreferredSize(_dimension);
-    JRootPane _rootPane = SwingUtilities.getRootPane(this.runButton);
-    _rootPane.setDefaultButton(this.runButton);
-  }
-  
-  private void addParam(final String label, final String text, final Component component, final int height, final double weighty) {
-    this.paramPos++;
-    final GridBagConstraints c = new GridBagConstraints();
-    final JLabel paramLabel = new JLabel(label);
-    c.gridx = 0;
-    c.gridy = this.paramPos;
-    c.gridwidth = 1;
-    Insets _insets = new Insets(10, 10, 0, 0);
-    c.insets = _insets;
-    c.anchor = GridBagConstraints.NORTHWEST;
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0;
-    c.weighty = 0;
-    this.paneParams.add(paramLabel, c);
-    c.gridx = 1;
-    c.gridwidth = GridBagConstraints.REMAINDER;
-    c.anchor = GridBagConstraints.WEST;
-    c.fill = GridBagConstraints.BOTH;
-    Insets _insets_1 = new Insets(10, 10, 0, 10);
-    c.insets = _insets_1;
-    c.weightx = 1;
-    c.weighty = weighty;
-    if ((component instanceof JTextField)) {
-      ((JTextField)component).setText(text);
-      this.paneParams.add(component, c);
-    } else {
-      if ((component instanceof JTextArea)) {
-        ((JTextArea)component).setText(text);
-        ((JTextArea)component).setLineWrap(true);
-        ((JTextArea)component).setWrapStyleWord(true);
-        JScrollPane scrollPane = new JScrollPane(component);
-        JViewport _viewport = scrollPane.getViewport();
-        Dimension _dimension = new Dimension(200, height);
-        _viewport.setPreferredSize(_dimension);
-        this.paneParams.add(scrollPane, c);
-      }
+
+    @Override
+    public void focusLost(final FocusEvent e) {
+        // ignore
     }
-    component.addFocusListener(this);
-  }
-  
-  public void exit() {
-    WindowEvent _windowEvent = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
-    this.dispatchEvent(_windowEvent);
-  }
-  
-  @Override
-  public void actionPerformed(final ActionEvent e) {
-    try {
-      Object _source = e.getSource();
-      boolean _equals = Objects.equal(_source, this.runButton);
-      if (_equals) {
-        this.reporter.setSchemas(this.schemasTextField.getText());
-        this.reporter.setIncludeObjects(this.includeObjectsTextArea.getText());
-        this.reporter.setExcludeObjects(this.excludeObjectsTextArea.getText());
-        this.schemasTextField.setEnabled(false);
-        this.includeObjectsTextArea.setEnabled(false);
-        this.excludeObjectsTextArea.setEnabled(false);
-        this.runButton.setEnabled(false);
-        this.reporter.runAsync();
-      } else {
-        Object _source_1 = e.getSource();
-        boolean _equals_1 = Objects.equal(_source_1, this.cancelButton);
-        if (_equals_1) {
-          boolean _isEnabled = this.runButton.isEnabled();
-          if (_isEnabled) {
-            this.exit();
-          } else {
-            Connection _connection = this.reporter.getConnection();
-            SimpleAsyncTaskExecutor _simpleAsyncTaskExecutor = new SimpleAsyncTaskExecutor();
-            _connection.abort(_simpleAsyncTaskExecutor);
-          }
-        }
-      }
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
-    }
-  }
-  
-  @Override
-  public void focusGained(final FocusEvent e) {
-    boolean _isAncestorOf = this.paneParams.isAncestorOf(e.getComponent());
-    if (_isAncestorOf) {
-      final int x = (e.getComponent().getLocationOnScreen().x - this.paneParams.getLocationOnScreen().x);
-      final int y = (e.getComponent().getLocationOnScreen().y - this.paneParams.getLocationOnScreen().y);
-      final int width = e.getComponent().getBounds().width;
-      final int height = e.getComponent().getBounds().height;
-      final Rectangle rect = new Rectangle(x, y, width, height);
-      this.paneParams.scrollRectToVisible(rect);
-    }
-  }
-  
-  @Override
-  public void focusLost(final FocusEvent e) {
-  }
 }
