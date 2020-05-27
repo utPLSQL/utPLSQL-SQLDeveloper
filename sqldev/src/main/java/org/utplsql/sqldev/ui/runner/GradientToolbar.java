d561b70670a049f36b17f0bea407ef64c7e19ec4
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-27 11:59:03 +0200

    convert GradientToolbar to Java, removing Xtend dependencies

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/GradientToolbar.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/GradientToolbar.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,81 +15,71 @@
  */
 package org.utplsql.sqldev.ui.runner;
 
-import com.google.common.base.Objects;
 import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
+
 import javax.swing.JToolBar;
 import javax.swing.LookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 
-@SuppressWarnings("all")
 public class GradientToolbar extends JToolBar {
-  private boolean isOracleLookAndFeel() {
-    LookAndFeel _lookAndFeel = UIManager.getLookAndFeel();
-    String _name = null;
-    if (_lookAndFeel!=null) {
-      _name=_lookAndFeel.getName();
+    private static final long serialVersionUID = 6128707792081725058L;
+
+    private boolean isOracleLookAndFeel() {
+        LookAndFeel laf = UIManager.getLookAndFeel();
+        final String lafName = laf != null ? laf.getName() : null;
+        return "Oracle Look and Feel version 2".equals(lafName);
     }
-    final String laf = _name;
-    boolean _equals = Objects.equal(laf, "Oracle Look and Feel version 2");
-    if (_equals) {
-      return true;
-    } else {
-      return false;
+
+    public GradientToolbar() {
+        super();
+        if (isOracleLookAndFeel()) {
+            setBorder(new EmptyBorder(new Insets(2, 2, 2, 2))); // insets: top, left, bottom, right
+        } else {
+            setBorder(new BevelBorder(BevelBorder.RAISED));
+        }
     }
-  }
-  
-  public GradientToolbar() {
-    super();
-    boolean _isOracleLookAndFeel = this.isOracleLookAndFeel();
-    if (_isOracleLookAndFeel) {
-      Insets _insets = new Insets(2, 2, 2, 2);
-      EmptyBorder _emptyBorder = new EmptyBorder(_insets);
-      this.setBorder(_emptyBorder);
-    } else {
-      BevelBorder _bevelBorder = new BevelBorder(BevelBorder.RAISED);
-      this.setBorder(_bevelBorder);
+
+    @Override
+    public void paintComponent(final Graphics g) {
+        if (isOracleLookAndFeel()) {
+            // emulate Oracle toolbar
+            // 1. default for non-opaque components
+            if (!isOpaque()) {
+                super.paintComponent(g);
+                return;
+            }
+
+            // 2. paint gradient background from top to bottom with separator line at the bottom
+            final Graphics2D g2d = ((Graphics2D) g);
+            final int w = getWidth();
+            final int h = (getHeight() - 1);
+            final int h2 = (getHeight() / 2);
+            final Color colorTop = new Color(237, 237, 237);
+            final Color colorMiddle = new Color(244, 244, 244);
+            final Color colorBottom = new Color(254, 254, 254);
+            final Color colorBottomLine = Color.LIGHT_GRAY;
+            final GradientPaint gp1 = new GradientPaint(0, 0, colorTop, 0, h2, colorMiddle);
+            g2d.setPaint(gp1);
+            g2d.fillRect(0, 0, w, h2);
+            final GradientPaint gp2 = new GradientPaint(0, h2, colorMiddle, 0, h, colorBottom);
+            g2d.setPaint(gp2);
+            g2d.fillRect(0, h2, w, h);
+            g2d.setPaint(colorBottomLine);
+            g2d.fillRect(0, h, w, (h + 1));
+
+            // 3. do rest, changing opaque to ensure background is not overwritten
+            setOpaque(false);
+            super.paintComponent(g);
+            setOpaque(true);
+        } else {
+            // default logic
+            super.paintComponent(g);
+        }
     }
-  }
-  
-  @Override
-  public void paintComponent(final Graphics g) {
-    boolean _isOracleLookAndFeel = this.isOracleLookAndFeel();
-    if (_isOracleLookAndFeel) {
-      boolean _isOpaque = this.isOpaque();
-      boolean _not = (!_isOpaque);
-      if (_not) {
-        super.paintComponent(g);
-        return;
-      }
-      final Graphics2D g2d = ((Graphics2D) g);
-      final int w = this.getWidth();
-      int _height = this.getHeight();
-      final int h = (_height - 1);
-      int _height_1 = this.getHeight();
-      final int h2 = (_height_1 / 2);
-      final Color colorTop = new Color(237, 237, 237);
-      final Color colorMiddle = new Color(244, 244, 244);
-      final Color colorBottom = new Color(254, 254, 254);
-      final Color colorBottomLine = Color.LIGHT_GRAY;
-      final GradientPaint gp1 = new GradientPaint(0, 0, colorTop, 0, h2, colorMiddle);
-      g2d.setPaint(gp1);
-      g2d.fillRect(0, 0, w, h2);
-      final GradientPaint gp2 = new GradientPaint(0, h2, colorMiddle, 0, h, colorBottom);
-      g2d.setPaint(gp2);
-      g2d.fillRect(0, h2, w, h);
-      g2d.setPaint(colorBottomLine);
-      g2d.fillRect(0, h, w, (h + 1));
-      this.setOpaque(false);
-      super.paintComponent(g);
-      this.setOpaque(true);
-    } else {
-      super.paintComponent(g);
-    }
-  }
 }
