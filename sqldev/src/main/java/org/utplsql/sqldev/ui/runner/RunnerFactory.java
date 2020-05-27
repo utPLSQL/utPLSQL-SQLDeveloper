4498c417ca4d6711de813ed805edd158004036bd
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-27 12:00:38 +0200

    add RunnerFactory.java generated by Xtend 2.20.0

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerFactory.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerFactory.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,49 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.ui.runner
+package org.utplsql.sqldev.ui.runner;
 
-import oracle.ide.docking.DockStation
-import oracle.ide.docking.DockableFactory
-import oracle.ide.docking.DockingParam
-import oracle.ide.layout.ViewId
+import oracle.ide.docking.DockStation;
+import oracle.ide.docking.Dockable;
+import oracle.ide.docking.DockableFactory;
+import oracle.ide.docking.DockingParam;
+import oracle.ide.layout.ViewId;
+import org.utplsql.sqldev.ui.runner.RunnerView;
 
-class RunnerFactory implements DockableFactory {
-	public static val FACTORY_NAME = "UTPLSQL_RUNNER_FACTORY"
-	
-	var RunnerView dockable
-	
-	override install() {
-		val dockStation = DockStation.getDockStation();
-		val dp = new DockingParam();
-		val referencedViewId = new ViewId("DatabaseNavigatorWindow", "DatabaseNavigatorWindow")
-		val referencedDockable = dockStation.findDockable(referencedViewId)
-		dp.tabbedWith = referencedDockable
-		dockStation.dock(getLocalDockable(), dp);		
-	}
-
-	override getDockable(ViewId viewId) {
-		if (viewId === RunnerView.VIEW_ID) {
-			return localDockable
-		}
-		return null		
-	}
-	
-	private def getLocalDockable() {
-		if (dockable === null) {
-			dockable = new RunnerView
-		}
-		return dockable
-	}
-	
-	static def getDockable() {
-		val dockStation = DockStation.dockStation
-		val dockable = dockStation.findDockable(RunnerView.VIEW_ID)
-		return dockable as RunnerView
-	}
-
-	static def void showDockable() {
-		val dockStation = DockStation.dockStation
-		dockStation.setDockableVisible(getDockable(), true)
-	}	
+@SuppressWarnings("all")
+public class RunnerFactory implements DockableFactory {
+  public static final String FACTORY_NAME = "UTPLSQL_RUNNER_FACTORY";
+  
+  private RunnerView dockable;
+  
+  @Override
+  public void install() {
+    final DockStation dockStation = DockStation.getDockStation();
+    final DockingParam dp = new DockingParam();
+    final ViewId referencedViewId = new ViewId("DatabaseNavigatorWindow", "DatabaseNavigatorWindow");
+    final Dockable referencedDockable = dockStation.findDockable(referencedViewId);
+    dp.setTabbedWith(referencedDockable);
+    dockStation.dock(this.getLocalDockable(), dp);
+  }
+  
+  @Override
+  public Dockable getDockable(final ViewId viewId) {
+    if ((viewId == RunnerView.VIEW_ID)) {
+      return this.getLocalDockable();
+    }
+    return null;
+  }
+  
+  private RunnerView getLocalDockable() {
+    if ((this.dockable == null)) {
+      RunnerView _runnerView = new RunnerView();
+      this.dockable = _runnerView;
+    }
+    return this.dockable;
+  }
+  
+  public static RunnerView getDockable() {
+    final DockStation dockStation = DockStation.getDockStation();
+    final Dockable dockable = dockStation.findDockable(RunnerView.VIEW_ID);
+    return ((RunnerView) dockable);
+  }
+  
+  public static void showDockable() {
+    final DockStation dockStation = DockStation.getDockStation();
+    dockStation.setDockableVisible(RunnerFactory.getDockable(), true);
+  }
 }
