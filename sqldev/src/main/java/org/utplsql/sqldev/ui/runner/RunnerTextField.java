dd3f071e6f33c3c3b6ee85d7e88853945e287c6f
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 08:09:32 +0200

    add RunnerTextField.java generated by Xtend 2.20.0

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerTextField.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerTextField.java
@@ -1,39 +1,43 @@
-package org.utplsql.sqldev.ui.runner
+package org.utplsql.sqldev.ui.runner;
 
-import java.awt.Graphics
-import java.awt.event.FocusEvent
-import java.awt.event.FocusListener
-import javax.swing.JTextField
-import javax.swing.UIManager
+import java.awt.Graphics;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.text.Caret;
 
-class RunnerTextField extends JTextField implements FocusListener {
-	
-	new() {
-		super()
-		this.addFocusListener = this
-	}
-
-	override paintComponent(Graphics g) {
-		// default for non-opaque components
-		if (!opaque) {
-			super.paintComponent(g)
-			return
-		}
-
-		g.color = UIManager.getColor("TextField.inactiveBackground")
-		g.fillRect(0, 0, width, height)
-
-		// do rest, changing opaque to ensure background is not overwritten
-		setOpaque(false)
-		super.paintComponent(g)
-		setOpaque(true)
-	}
-
-	override void focusGained(FocusEvent e) {
-		this.caret.visible = true
-	}
-
-	override focusLost(FocusEvent e) {
-		this.caret.visible = false
-	}
+@SuppressWarnings("all")
+public class RunnerTextField extends JTextField implements FocusListener {
+  public RunnerTextField() {
+    super();
+    this.addFocusListener(this);
+  }
+  
+  @Override
+  public void paintComponent(final Graphics g) {
+    boolean _isOpaque = this.isOpaque();
+    boolean _not = (!_isOpaque);
+    if (_not) {
+      super.paintComponent(g);
+      return;
+    }
+    g.setColor(UIManager.getColor("TextField.inactiveBackground"));
+    g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    this.setOpaque(false);
+    super.paintComponent(g);
+    this.setOpaque(true);
+  }
+  
+  @Override
+  public void focusGained(final FocusEvent e) {
+    Caret _caret = this.getCaret();
+    _caret.setVisible(true);
+  }
+  
+  @Override
+  public void focusLost(final FocusEvent e) {
+    Caret _caret = this.getCaret();
+    _caret.setVisible(false);
+  }
 }
