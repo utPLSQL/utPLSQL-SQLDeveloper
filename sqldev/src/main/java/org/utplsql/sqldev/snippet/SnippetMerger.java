f6616548b00ee8944d461cb43cf5960085cd9bb2
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 19:41:10 +0200

    simplify, use methods from XMLTools

--- sqldev/src/main/java/org/utplsql/sqldev/snippet/SnippetMerger.java
+++ sqldev/src/main/java/org/utplsql/sqldev/snippet/SnippetMerger.java
@@ -17,33 +17,24 @@ package org.utplsql.sqldev.snippet;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
-import org.utplsql.sqldev.exception.GenericRuntimeException;
 import org.utplsql.sqldev.model.FileTools;
 import org.utplsql.sqldev.model.XMLTools;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import oracle.dbtools.util.Resource;
 
 public class SnippetMerger {
-    private static final Logger logger = Logger.getLogger(SnippetMerger.class.getName());
-
     private final XMLTools xmlTools = new XMLTools();
     private File userSnippetsFile;
     private String utplsqlSnippets;
@@ -65,38 +56,16 @@ public class SnippetMerger {
         userSnippetsFile = file;
     }
     
-    private DocumentBuilder createDocumentBuilder() {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, Boolean.TRUE);
-            return factory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            final String msg = "Could not create no document builder due to " + e.getMessage() + ".";
-            logger.severe(() -> msg);
-            throw new GenericRuntimeException(msg, e);
-        }
-    }
-    
-    private Document parse(final DocumentBuilder builder, final InputSource inputSource) {
-        try {
-            return builder.parse(inputSource);
-        } catch (SAXException | IOException e) {
-            final String msg = "Could not parse XML input due to " + e.getMessage() + ".";
-            logger.severe(() -> msg);
-            throw new GenericRuntimeException(msg, e);
-        }
-    }
-
     public void merge() {
         String result = null;
         if (userSnippetsFile.exists()) {
             // file exists, proper merge required
             final String userSnippets = new String(FileTools.readFile(Paths.get(userSnippetsFile.getAbsolutePath())));
-            final DocumentBuilder docBuilder = createDocumentBuilder();
-            final Document userSnippetsDoc = parse(docBuilder, new InputSource(new StringReader(userSnippets)));
+            final DocumentBuilder docBuilder = xmlTools.createDocumentBuilder();
+            final Document userSnippetsDoc = xmlTools.parse(docBuilder, new InputSource(new StringReader(userSnippets)));
             final NodeList userSnippetsGroups = xmlTools.getNodeList(userSnippetsDoc,
                     "/snippets/group[not(@category=\"utPLSQL Annotations\" or @category=\"utPLSQL Expectations\")]");
-            final Document utplsqlSnippetsDoc = parse(docBuilder, new InputSource(new StringReader(utplsqlSnippets)));
+            final Document utplsqlSnippetsDoc = xmlTools.parse(docBuilder, new InputSource(new StringReader(utplsqlSnippets)));
             final NodeList utplsqlSnippetsGroups = xmlTools.getNodeList(utplsqlSnippetsDoc, "/snippets/group");
             StringBuilder sb = new StringBuilder();
             sb.append("<?xml version = '1.0' encoding = 'UTF-8'?>\n");
