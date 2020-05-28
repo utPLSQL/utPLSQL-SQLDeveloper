feb329e06968c686bd584968f181e2529130bf68
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 10:08:29 +0200

    add ScrollablePanel.java generated by Xtend 2.20.0

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/ScrollablePanel.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/ScrollablePanel.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,35 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.ui.runner
+package org.utplsql.sqldev.ui.runner;
 
-import java.awt.Rectangle
-import javax.swing.JPanel
-import javax.swing.Scrollable
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import javax.swing.JPanel;
+import javax.swing.Scrollable;
 
-/*
- * Fixes resizing issues of JTextArea when put into JPanel and JPanel into JScrollPane 
+/**
+ * Fixes resizing issues of JTextArea when put into JPanel and JPanel into JScrollPane
  * Solution is based on https://stackoverflow.com/questions/15783014/jtextarea-on-jpanel-inside-jscrollpane-does-not-resize-properly/15786939
- */ 
-class ScrollablePanel extends JPanel implements Scrollable {
-
-	override getPreferredScrollableViewportSize() {
-		return super.getPreferredSize()
-	}
-
-	override getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
-		return 0
-	}
-
-	override getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
-		return 0
-	}
-
-	override getScrollableTracksViewportWidth() {
-		return true
-	}
-
-	override getScrollableTracksViewportHeight() {
-		return false
-	}
+ */
+@SuppressWarnings("all")
+public class ScrollablePanel extends JPanel implements Scrollable {
+  @Override
+  public Dimension getPreferredScrollableViewportSize() {
+    return super.getPreferredSize();
+  }
+  
+  @Override
+  public int getScrollableUnitIncrement(final Rectangle visibleRect, final int orientation, final int direction) {
+    return 0;
+  }
+  
+  @Override
+  public int getScrollableBlockIncrement(final Rectangle visibleRect, final int orientation, final int direction) {
+    return 0;
+  }
+  
+  @Override
+  public boolean getScrollableTracksViewportWidth() {
+    return true;
+  }
+  
+  @Override
+  public boolean getScrollableTracksViewportHeight() {
+    return false;
+  }
 }
