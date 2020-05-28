098a722253d623c0e293a68247f24ca8fe5e9b42
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 09:17:53 +0200

    add RunnerView.java generated by Xtend 2.20.0

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerView.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerView.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,34 +13,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.ui.runner
+package org.utplsql.sqldev.ui.runner;
 
-import oracle.ide.docking.DockableWindow
-import oracle.ide.layout.ViewId
-import org.utplsql.sqldev.resources.UtplsqlResources
+import java.awt.Component;
+import javax.swing.Icon;
+import oracle.ide.docking.DockableWindow;
+import oracle.ide.layout.ViewId;
+import org.utplsql.sqldev.resources.UtplsqlResources;
+import org.utplsql.sqldev.ui.runner.RunnerFactory;
+import org.utplsql.sqldev.ui.runner.RunnerPanel;
 
-class RunnerView extends DockableWindow {
-	static val VIEW_NAME = "UTPLSQL_RUNNER_VIEW"
-	public static val ViewId VIEW_ID = new ViewId(RunnerFactory.FACTORY_NAME, VIEW_NAME)
-	var RunnerPanel panel
-	
-	override getTitleName() {
-		return UtplsqlResources.getString("RUNNER_VIEW_TITLE")
-	}
-	
-	override getGUI() {
-		if (panel === null) {
-			panel = new RunnerPanel
-		}
-		return panel.getGUI()
-	}
-	
-	override getTabIcon() {
-		return UtplsqlResources.getIcon("UTPLSQL_ICON")
-	}
-	
-	def getRunnerPanel() {
-		getGUI()
-		return panel
-	}
+@SuppressWarnings("all")
+public class RunnerView extends DockableWindow {
+  private static final String VIEW_NAME = "UTPLSQL_RUNNER_VIEW";
+  
+  public static final ViewId VIEW_ID = new ViewId(RunnerFactory.FACTORY_NAME, RunnerView.VIEW_NAME);
+  
+  private RunnerPanel panel;
+  
+  @Override
+  public String getTitleName() {
+    return UtplsqlResources.getString("RUNNER_VIEW_TITLE");
+  }
+  
+  @Override
+  public Component getGUI() {
+    if ((this.panel == null)) {
+      RunnerPanel _runnerPanel = new RunnerPanel();
+      this.panel = _runnerPanel;
+    }
+    return this.panel.getGUI();
+  }
+  
+  @Override
+  public Icon getTabIcon() {
+    return UtplsqlResources.getIcon("UTPLSQL_ICON");
+  }
+  
+  public RunnerPanel getRunnerPanel() {
+    this.getGUI();
+    return this.panel;
+  }
 }
