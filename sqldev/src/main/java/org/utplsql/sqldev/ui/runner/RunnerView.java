d54e73fc1b862da265ec3f60bc00d4d9737259c4
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 10:01:51 +0200

    convert RunnerView to Java, removing Xtend dependencies

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerView.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerView.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,42 +16,39 @@
 package org.utplsql.sqldev.ui.runner;
 
 import java.awt.Component;
+
 import javax.swing.Icon;
+
+import org.utplsql.sqldev.resources.UtplsqlResources;
+
 import oracle.ide.docking.DockableWindow;
 import oracle.ide.layout.ViewId;
-import org.utplsql.sqldev.resources.UtplsqlResources;
-import org.utplsql.sqldev.ui.runner.RunnerFactory;
-import org.utplsql.sqldev.ui.runner.RunnerPanel;
 
-@SuppressWarnings("all")
 public class RunnerView extends DockableWindow {
-  private static final String VIEW_NAME = "UTPLSQL_RUNNER_VIEW";
-  
-  public static final ViewId VIEW_ID = new ViewId(RunnerFactory.FACTORY_NAME, RunnerView.VIEW_NAME);
-  
-  private RunnerPanel panel;
-  
-  @Override
-  public String getTitleName() {
-    return UtplsqlResources.getString("RUNNER_VIEW_TITLE");
-  }
-  
-  @Override
-  public Component getGUI() {
-    if ((this.panel == null)) {
-      RunnerPanel _runnerPanel = new RunnerPanel();
-      this.panel = _runnerPanel;
+    private static final String VIEW_NAME = "UTPLSQL_RUNNER_VIEW";
+    public static final ViewId VIEW_ID = new ViewId(RunnerFactory.FACTORY_NAME, VIEW_NAME);
+    private RunnerPanel panel;
+
+    @Override
+    public String getTitleName() {
+        return UtplsqlResources.getString("RUNNER_VIEW_TITLE");
     }
-    return this.panel.getGUI();
-  }
-  
-  @Override
-  public Icon getTabIcon() {
-    return UtplsqlResources.getIcon("UTPLSQL_ICON");
-  }
-  
-  public RunnerPanel getRunnerPanel() {
-    this.getGUI();
-    return this.panel;
-  }
+
+    @Override
+    public Component getGUI() {
+        if (panel == null) {
+            panel = new RunnerPanel();
+        }
+        return panel.getGUI();
+    }
+
+    @Override
+    public Icon getTabIcon() {
+        return UtplsqlResources.getIcon("UTPLSQL_ICON");
+    }
+
+    public RunnerPanel getRunnerPanel() {
+        getGUI();
+        return panel;
+    }
 }
