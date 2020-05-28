1f89b8eb0fd5e3c7bf675df1742b9331eda2658a
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 16:47:52 +0200

    convert CodeCoverageReporterTest to Java, removing Xtend dependencies

--- sqldev/src/test/java/org/utplsql/sqldev/test/coverage/CodeCoverageReporterTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/coverage/CodeCoverageReporterTest.java
@@ -16,158 +16,110 @@
 package org.utplsql.sqldev.test.coverage;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.function.ToLongFunction;
-import org.eclipse.xtend2.lib.StringConcatenation;
-import org.eclipse.xtext.xbase.lib.CollectionLiterals;
-import org.eclipse.xtext.xbase.lib.Exceptions;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.utplsql.sqldev.coverage.CodeCoverageReporter;
+import org.utplsql.sqldev.exception.GenericRuntimeException;
+import org.utplsql.sqldev.model.DatabaseTools;
+import org.utplsql.sqldev.model.FileTools;
+import org.utplsql.sqldev.model.SystemTools;
 import org.utplsql.sqldev.test.AbstractJdbcTest;
 
-@SuppressWarnings("all")
 public class CodeCoverageReporterTest extends AbstractJdbcTest {
-  @BeforeClass
-  public static void setup() {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("CREATE OR REPLACE FUNCTION f RETURN INTEGER IS");
-    _builder.newLine();
-    _builder.append("BEGIN");
-    _builder.newLine();
-    _builder.append("   ");
-    _builder.append("RETURN 1;");
-    _builder.newLine();
-    _builder.append("END f;");
-    _builder.newLine();
-    AbstractJdbcTest.jdbcTemplate.execute(_builder.toString());
-    StringConcatenation _builder_1 = new StringConcatenation();
-    _builder_1.append("CREATE OR REPLACE PACKAGE test_f IS");
-    _builder_1.newLine();
-    _builder_1.append("   ");
-    _builder_1.append("--%suite");
-    _builder_1.newLine();
-    _builder_1.newLine();
-    _builder_1.append("   ");
-    _builder_1.append("--%test");
-    _builder_1.newLine();
-    _builder_1.append("   ");
-    _builder_1.append("PROCEDURE f;");
-    _builder_1.newLine();
-    _builder_1.append("END test_f;");
-    _builder_1.newLine();
-    AbstractJdbcTest.jdbcTemplate.execute(_builder_1.toString());
-    StringConcatenation _builder_2 = new StringConcatenation();
-    _builder_2.append("CREATE OR REPLACE PACKAGE BODY test_f IS");
-    _builder_2.newLine();
-    _builder_2.append("   ");
-    _builder_2.append("--%test");
-    _builder_2.newLine();
-    _builder_2.append("   ");
-    _builder_2.append("PROCEDURE f IS");
-    _builder_2.newLine();
-    _builder_2.append("      ");
-    _builder_2.append("l_expected INTEGER := 1;");
-    _builder_2.newLine();
-    _builder_2.append("      ");
-    _builder_2.append("l_actual   INTEGER;");
-    _builder_2.newLine();
-    _builder_2.append("   ");
-    _builder_2.append("BEGIN");
-    _builder_2.newLine();
-    _builder_2.append("      ");
-    _builder_2.append("l_actual := scott.f();");
-    _builder_2.newLine();
-    _builder_2.append("      ");
-    _builder_2.append("ut.expect(l_actual).to_equal(l_expected);");
-    _builder_2.newLine();
-    _builder_2.append("   ");
-    _builder_2.append("END f;");
-    _builder_2.newLine();
-    _builder_2.append("END test_f;");
-    _builder_2.newLine();
-    AbstractJdbcTest.jdbcTemplate.execute(_builder_2.toString());
-  }
-  
-  private Path getNewestOutputFile() {
-    try {
-      final File file = File.createTempFile("test", ".txt");
-      final File dir = file.getParentFile();
-      file.delete();
-      final Predicate<Path> _function = (Path f) -> {
-        boolean _isDirectory = f.toFile().isDirectory();
-        return (!_isDirectory);
-      };
-      final Predicate<Path> _function_1 = (Path f) -> {
-        return f.getFileName().toString().startsWith("utplsql_");
-      };
-      final Predicate<Path> _function_2 = (Path f) -> {
-        return f.getFileName().toString().endsWith(".html");
-      };
-      final ToLongFunction<Path> _function_3 = (Path f) -> {
-        return f.toFile().lastModified();
-      };
-      final Optional<Path> last = Files.list(dir.toPath()).filter(_function).filter(_function_1).filter(_function_2).max(Comparator.<Path>comparingLong(_function_3));
-      return last.get();
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
+
+    @BeforeClass
+    public static void setup() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE OR REPLACE FUNCTION f RETURN INTEGER IS\n");
+        sb.append("BEGIN\n");
+        sb.append("   RETURN 1;\n");
+        sb.append("END f;");
+        jdbcTemplate.execute(sb.toString());
+        sb.setLength(0);
+        sb.append("CREATE OR REPLACE PACKAGE test_f IS\n");
+        sb.append("   --%suite\n\n");
+        sb.append("   --%test\n");
+        sb.append("   PROCEDURE f;\n");
+        sb.append("END test_f;");
+        jdbcTemplate.execute(sb.toString());
+        sb.setLength(0);
+        sb.append("CREATE OR REPLACE PACKAGE BODY test_f IS\n");
+        sb.append("   --%test\n");
+        sb.append("   PROCEDURE f IS\n");
+        sb.append("      l_expected INTEGER := 1;\n");
+        sb.append("      l_actual   INTEGER;\n");
+        sb.append("   BEGIN\n");
+        sb.append("      l_actual := scott.f();\n");
+        sb.append("      ut.expect(l_actual).to_equal(l_expected);\n");
+        sb.append("   END f;\n");
+        sb.append("END test_f;");
+        jdbcTemplate.execute(sb.toString());
     }
-  }
-  
-  @Test
-  public void produceReportAndCloseConnection() {
-    try {
-      SingleConnectionDataSource ds = new SingleConnectionDataSource();
-      ds.setDriverClassName("oracle.jdbc.OracleDriver");
-      ds.setUrl(AbstractJdbcTest.dataSource.getUrl());
-      ds.setUsername(AbstractJdbcTest.dataSource.getUsername());
-      ds.setPassword(AbstractJdbcTest.dataSource.getPassword());
-      final Connection conn = ds.getConnection();
-      final List<String> pathList = Collections.<String>unmodifiableList(CollectionLiterals.<String>newArrayList(":test_f"));
-      final List<String> includeObjectList = Collections.<String>unmodifiableList(CollectionLiterals.<String>newArrayList("f"));
-      final CodeCoverageReporter reporter = new CodeCoverageReporter(pathList, includeObjectList, conn);
-      final Thread run = reporter.runAsync();
-      run.join(20000);
-      Assert.assertEquals(Boolean.valueOf(true), Boolean.valueOf(conn.isClosed()));
-      final Path outputFile = this.getNewestOutputFile();
-      Assert.assertTrue((outputFile != null));
-      byte[] _readAllBytes = Files.readAllBytes(outputFile);
-      final String content = new String(_readAllBytes, StandardCharsets.UTF_8);
-      Assert.assertTrue(content.contains("<h3>SCOTT.F</h3><h4><span class=\"green\">100 %</span> lines covered</h4>"));
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
+    
+    private File createTempFile(String prefix, String suffix) {
+        try {
+            return File.createTempFile("test", ".txt");
+        } catch (IOException e) {
+            final String msg = "Cannot create temporary file with prefix '" + prefix + "' and suffix '" + suffix + "'.";
+            throw new GenericRuntimeException(msg, e);
+        }
     }
-  }
-  
-  @AfterClass
-  public static void teardown() {
-    try {
-      AbstractJdbcTest.jdbcTemplate.execute("DROP PACKAGE test_f");
-    } catch (final Throwable _t) {
-      if (_t instanceof BadSqlGrammarException) {
-      } else {
-        throw Exceptions.sneakyThrow(_t);
-      }
+
+    private Path getNewestOutputFile() {
+            final File file = createTempFile("test", ".txt");
+            final File dir = file.getParentFile();
+            file.delete();
+            Optional<Path> last;
+            try {
+                last = Files.list(dir.toPath())
+                        .filter(f -> !f.toFile().isDirectory() && f.getFileName().toString().startsWith("utplsql_")
+                                && f.getFileName().toString().endsWith(".html"))
+                        .max(Comparator.comparingLong(f -> f.toFile().lastModified()));
+            } catch (IOException e) {
+                final String msg = "Cannot get newest output file in " + dir.getAbsolutePath() + ".";
+                throw new GenericRuntimeException(msg, e);
+            }
+            return last.get();
     }
-    try {
-      AbstractJdbcTest.jdbcTemplate.execute("DROP FUNCTION f");
-    } catch (final Throwable _t_1) {
-      if (_t_1 instanceof BadSqlGrammarException) {
-      } else {
-        throw Exceptions.sneakyThrow(_t_1);
-      }
+
+    @Test
+    public void produceReportAndCloseConnection() {
+        // create temporary dataSource, closed by reporter
+        SingleConnectionDataSource ds = new SingleConnectionDataSource();
+        ds.setDriverClassName("oracle.jdbc.OracleDriver");
+        ds.setUrl(dataSource.getUrl());
+        ds.setUsername(dataSource.getUsername());
+        ds.setPassword(dataSource.getPassword());
+        final Connection conn = DatabaseTools.getConnection(ds);
+        final List<String> pathList = Arrays.asList(":test_f");
+        final List<String> includeObjectList = Arrays.asList("f");
+        final CodeCoverageReporter reporter = new CodeCoverageReporter(pathList, includeObjectList, conn);
+        final Thread run = reporter.runAsync();
+        SystemTools.waitForThread(run, 20000);
+        Assert.assertTrue(DatabaseTools.isConnectionClosed(conn));
+        final Path outputFile = this.getNewestOutputFile();
+        Assert.assertNotNull(outputFile);
+        final String content = new String(FileTools.readFile(outputFile), StandardCharsets.UTF_8);
+        Assert.assertTrue(
+                content.contains("<h3>SCOTT.F</h3><h4><span class=\"green\">100 %</span> lines covered</h4>"));
     }
-  }
+
+    @AfterClass
+    public static void teardown() {
+        executeAndIgnore(jdbcTemplate, "DROP PACKAGE test_f");
+        executeAndIgnore(jdbcTemplate, "DROP FUNCTION f");
+    }
 }
