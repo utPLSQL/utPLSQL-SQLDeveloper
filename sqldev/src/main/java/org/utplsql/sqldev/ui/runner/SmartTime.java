ab6b036b55425ef322ab6ebc94df3acff5b149f2
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 10:12:33 +0200

    add SmartTime.java generated by Xtend 2.20.0

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/SmartTime.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/SmartTime.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,60 +13,75 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.ui.runner
+package org.utplsql.sqldev.ui.runner;
 
-import java.text.DecimalFormat
+import java.text.DecimalFormat;
 
-class SmartTime {
-	var Double seconds
-	var boolean smart = false
-	
-	new() {
-		super()
-	}
-	
-	new(Double seconds, boolean smart) {
-		super()
-		this.seconds = seconds
-		this.smart = smart
-	}
-	
-	def setSeconds(Double seconds) {
-		this.seconds = seconds
-	}
-	
-	def setSmart(boolean smart) {
-		this.smart = smart
-	}
-	
-	def getSeconds() {
-		return seconds
-	} 
-	
-	override toString() {
-		var String ret;
-		if (seconds === null) {
-			ret = null
-		} else if (smart) {
-			if (seconds >= 60*60) {
-				val DecimalFormat formatter = new DecimalFormat("#0.00")
-				ret = formatter.format(seconds / 60 / 60) + " h"
-			} else if (seconds >= 60) {
-				val DecimalFormat formatter = new DecimalFormat("#0.00")
-				ret = formatter.format(seconds / 60) + " min"
-			} else if (seconds >= 1) {
-				val DecimalFormat formatter = new DecimalFormat("#0.000")
-				ret = formatter.format(seconds) + " s"
-			} else {
-				val DecimalFormat formatter = new DecimalFormat("##0")
-				ret = formatter.format(seconds * 1000) + " ms"				
-			}
-			
-		} else {
-			val DecimalFormat formatter = new DecimalFormat("##,##0.000")
-			ret = formatter.format(seconds)
-		}
-		return ret
-	}
-
+@SuppressWarnings("all")
+public class SmartTime {
+  private Double seconds;
+  
+  private boolean smart = false;
+  
+  public SmartTime() {
+    super();
+  }
+  
+  public SmartTime(final Double seconds, final boolean smart) {
+    super();
+    this.seconds = seconds;
+    this.smart = smart;
+  }
+  
+  public Double setSeconds(final Double seconds) {
+    return this.seconds = seconds;
+  }
+  
+  public boolean setSmart(final boolean smart) {
+    return this.smart = smart;
+  }
+  
+  public Double getSeconds() {
+    return this.seconds;
+  }
+  
+  @Override
+  public String toString() {
+    String ret = null;
+    if ((this.seconds == null)) {
+      ret = null;
+    } else {
+      if (this.smart) {
+        if (((this.seconds).doubleValue() >= (60 * 60))) {
+          final DecimalFormat formatter = new DecimalFormat("#0.00");
+          String _format = formatter.format((((this.seconds).doubleValue() / 60) / 60));
+          String _plus = (_format + " h");
+          ret = _plus;
+        } else {
+          if (((this.seconds).doubleValue() >= 60)) {
+            final DecimalFormat formatter_1 = new DecimalFormat("#0.00");
+            String _format_1 = formatter_1.format(((this.seconds).doubleValue() / 60));
+            String _plus_1 = (_format_1 + " min");
+            ret = _plus_1;
+          } else {
+            if (((this.seconds).doubleValue() >= 1)) {
+              final DecimalFormat formatter_2 = new DecimalFormat("#0.000");
+              String _format_2 = formatter_2.format(this.seconds);
+              String _plus_2 = (_format_2 + " s");
+              ret = _plus_2;
+            } else {
+              final DecimalFormat formatter_3 = new DecimalFormat("##0");
+              String _format_3 = formatter_3.format(((this.seconds).doubleValue() * 1000));
+              String _plus_3 = (_format_3 + " ms");
+              ret = _plus_3;
+            }
+          }
+        }
+      } else {
+        final DecimalFormat formatter_4 = new DecimalFormat("##,##0.000");
+        ret = formatter_4.format(this.seconds);
+      }
+    }
+    return ret;
+  }
 }
