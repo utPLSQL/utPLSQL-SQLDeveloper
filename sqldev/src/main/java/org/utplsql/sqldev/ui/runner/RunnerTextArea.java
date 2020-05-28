3450897e5544bda434a61599163e238bddd76667
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-28 08:00:10 +0200

    add RunnerTextArea.java generated by Xtend 2.20.0

--- sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerTextArea.java
+++ sqldev/src/main/java/org/utplsql/sqldev/ui/runner/RunnerTextArea.java
@@ -1,40 +1,47 @@
-package org.utplsql.sqldev.ui.runner
+package org.utplsql.sqldev.ui.runner;
 
-import java.awt.Graphics
-import java.awt.event.FocusEvent
-import java.awt.event.FocusListener
-import javax.swing.JTextArea
-import javax.swing.UIManager
+import java.awt.Graphics;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.text.Caret;
 
-class RunnerTextArea extends JTextArea implements FocusListener{
-
-	new() {
-		super()
-		this.addFocusListener = this
-	}
-
-	override paintComponent(Graphics g) {
-		// default for non-opaque components
-		if (!opaque) {
-			super.paintComponent(g)
-			return
-		}
-
-		// use value of JTextField for consistency
-		g.color = UIManager.getColor("TextField.inactiveBackground")
-		g.fillRect(3, 3, width - 6, height - 6)
-
-		// do rest, changing opaque to ensure background is not overwritten
-		setOpaque(false)
-		super.paintComponent(g)
-		setOpaque(true)
-	}
-
-	override void focusGained(FocusEvent e) {
-		this.caret.visible = true
-	}
-
-	override focusLost(FocusEvent e) {
-		this.caret.visible = false
-	}
+@SuppressWarnings("all")
+public class RunnerTextArea extends JTextArea implements FocusListener {
+  public RunnerTextArea() {
+    super();
+    this.addFocusListener(this);
+  }
+  
+  @Override
+  public void paintComponent(final Graphics g) {
+    boolean _isOpaque = this.isOpaque();
+    boolean _not = (!_isOpaque);
+    if (_not) {
+      super.paintComponent(g);
+      return;
+    }
+    g.setColor(UIManager.getColor("TextField.inactiveBackground"));
+    int _width = this.getWidth();
+    int _minus = (_width - 6);
+    int _height = this.getHeight();
+    int _minus_1 = (_height - 6);
+    g.fillRect(3, 3, _minus, _minus_1);
+    this.setOpaque(false);
+    super.paintComponent(g);
+    this.setOpaque(true);
+  }
+  
+  @Override
+  public void focusGained(final FocusEvent e) {
+    Caret _caret = this.getCaret();
+    _caret.setVisible(true);
+  }
+  
+  @Override
+  public void focusLost(final FocusEvent e) {
+    Caret _caret = this.getCaret();
+    _caret.setVisible(false);
+  }
 }
