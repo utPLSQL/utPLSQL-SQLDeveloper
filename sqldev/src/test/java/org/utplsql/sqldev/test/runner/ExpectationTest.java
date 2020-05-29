42ab75940ac4fb98a134da6724a1fd0cc6167993
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-29 09:34:09 +0200

    add ExpectationTest generated by Xtend 2.20.0

--- sqldev/src/test/java/org/utplsql/sqldev/test/runner/ExpectationTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/runner/ExpectationTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,49 +13,63 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.test.runner
+package org.utplsql.sqldev.test.runner;
 
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
-import org.utplsql.sqldev.model.runner.Expectation
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.utplsql.sqldev.model.runner.Expectation;
 
-class ExpectationTest {
-	var Expectation exceptionWithDescription
-	var Expectation exceptionWithoutDescription
-	
-	@Before
-	def void setup() {
-		exceptionWithDescription = new Expectation
-		exceptionWithDescription.description = '''This assert must fail'''
-		exceptionWithDescription.message = '''at: 1 (number) was expected to equal: 2 (number)'''
-		exceptionWithDescription.caller  = '''"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_2_NOK", line 14 ut.expect(1, 'This assert must fail').to_equal(2);'''
-		exceptionWithoutDescription = new Expectation
-		exceptionWithoutDescription.message = exceptionWithDescription.message
-		exceptionWithoutDescription.caller = exceptionWithDescription.caller
-		exceptionWithoutDescription.message = '''at: 1 (number) was expected to equal: 2 (number)'''
-		exceptionWithoutDescription.caller  = '''"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_3_NOK", line 42 ut.expect(1).to_equal(2);'''
-	}
-	
-	@Test
-	def void failedExpectationCallerLine() {
-		val actual = exceptionWithDescription.callerLine
-		val expected = new Integer(14)
-		Assert.assertEquals(expected, actual)
-	}
-
-	@Test
-	def void shortFailureTextWithDescription() {
-		val actual = exceptionWithDescription.shortFailureText
-		val expected = 'This assert must fail (line 14)'
-		Assert.assertEquals(expected, actual)
-	}
-
-	@Test
-	def void shortFailureTextWithoutDescription() {
-		val actual = exceptionWithoutDescription.shortFailureText
-		val expected = 'Line 42'
-		Assert.assertEquals(expected, actual)
-	}
-
+@SuppressWarnings("all")
+public class ExpectationTest {
+  private Expectation exceptionWithDescription;
+  
+  private Expectation exceptionWithoutDescription;
+  
+  @Before
+  public void setup() {
+    Expectation _expectation = new Expectation();
+    this.exceptionWithDescription = _expectation;
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("This assert must fail");
+    this.exceptionWithDescription.setDescription(_builder.toString());
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("at: 1 (number) was expected to equal: 2 (number)");
+    this.exceptionWithDescription.setMessage(_builder_1.toString());
+    StringConcatenation _builder_2 = new StringConcatenation();
+    _builder_2.append("\"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_2_NOK\", line 14 ut.expect(1, \'This assert must fail\').to_equal(2);");
+    this.exceptionWithDescription.setCaller(_builder_2.toString());
+    Expectation _expectation_1 = new Expectation();
+    this.exceptionWithoutDescription = _expectation_1;
+    this.exceptionWithoutDescription.setMessage(this.exceptionWithDescription.getMessage());
+    this.exceptionWithoutDescription.setCaller(this.exceptionWithDescription.getCaller());
+    StringConcatenation _builder_3 = new StringConcatenation();
+    _builder_3.append("at: 1 (number) was expected to equal: 2 (number)");
+    this.exceptionWithoutDescription.setMessage(_builder_3.toString());
+    StringConcatenation _builder_4 = new StringConcatenation();
+    _builder_4.append("\"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_3_NOK\", line 42 ut.expect(1).to_equal(2);");
+    this.exceptionWithoutDescription.setCaller(_builder_4.toString());
+  }
+  
+  @Test
+  public void failedExpectationCallerLine() {
+    final Integer actual = this.exceptionWithDescription.getCallerLine();
+    final Integer expected = new Integer(14);
+    Assert.assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void shortFailureTextWithDescription() {
+    final String actual = this.exceptionWithDescription.getShortFailureText();
+    final String expected = "This assert must fail (line 14)";
+    Assert.assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void shortFailureTextWithoutDescription() {
+    final String actual = this.exceptionWithoutDescription.getShortFailureText();
+    final String expected = "Line 42";
+    Assert.assertEquals(expected, actual);
+  }
 }
