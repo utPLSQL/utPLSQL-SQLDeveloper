9aab4fd28ec0da02ae850682da773172ba8d3a8c
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-29 09:43:50 +0200

    convert ExpectationTest to Java, removing Xtend dependencies

--- sqldev/src/test/java/org/utplsql/sqldev/test/runner/ExpectationTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/runner/ExpectationTest.java
@@ -15,61 +15,45 @@
  */
 package org.utplsql.sqldev.test.runner;
 
-import org.eclipse.xtend2.lib.StringConcatenation;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.utplsql.sqldev.model.runner.Expectation;
 
-@SuppressWarnings("all")
 public class ExpectationTest {
-  private Expectation exceptionWithDescription;
+    
+    private Expectation getExpectationWithDescription() {
+        Expectation ex = new Expectation();
+        ex.setDescription("This assert must fail");
+        ex.setMessage("at: 1 (number) was expected to equal: 2 (number)");
+        ex.setCaller("\"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_2_NOK\", line 14 ut.expect(1, 'This assert must fail').to_equal(2);");
+        return ex;
+    }
+
+    private Expectation getExpectationWithoutDescription() {
+        Expectation ex = new Expectation();
+        ex.setMessage("at: 1 (number) was expected to equal: 2 (number)");
+        ex.setCaller("\"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_3_NOK\", line 42 ut.expect(1).to_equal(2);");
+        return ex;
+    }
   
-  private Expectation exceptionWithoutDescription;
-  
-  @Before
-  public void setup() {
-    Expectation _expectation = new Expectation();
-    this.exceptionWithDescription = _expectation;
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("This assert must fail");
-    this.exceptionWithDescription.setDescription(_builder.toString());
-    StringConcatenation _builder_1 = new StringConcatenation();
-    _builder_1.append("at: 1 (number) was expected to equal: 2 (number)");
-    this.exceptionWithDescription.setMessage(_builder_1.toString());
-    StringConcatenation _builder_2 = new StringConcatenation();
-    _builder_2.append("\"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_2_NOK\", line 14 ut.expect(1, \'This assert must fail\').to_equal(2);");
-    this.exceptionWithDescription.setCaller(_builder_2.toString());
-    Expectation _expectation_1 = new Expectation();
-    this.exceptionWithoutDescription = _expectation_1;
-    this.exceptionWithoutDescription.setMessage(this.exceptionWithDescription.getMessage());
-    this.exceptionWithoutDescription.setCaller(this.exceptionWithDescription.getCaller());
-    StringConcatenation _builder_3 = new StringConcatenation();
-    _builder_3.append("at: 1 (number) was expected to equal: 2 (number)");
-    this.exceptionWithoutDescription.setMessage(_builder_3.toString());
-    StringConcatenation _builder_4 = new StringConcatenation();
-    _builder_4.append("\"SCOTT.JUNIT_UTPLSQL_TEST1_PKG.TEST_3_NOK\", line 42 ut.expect(1).to_equal(2);");
-    this.exceptionWithoutDescription.setCaller(_builder_4.toString());
-  }
-  
-  @Test
-  public void failedExpectationCallerLine() {
-    final Integer actual = this.exceptionWithDescription.getCallerLine();
-    final Integer expected = new Integer(14);
-    Assert.assertEquals(expected, actual);
-  }
-  
-  @Test
-  public void shortFailureTextWithDescription() {
-    final String actual = this.exceptionWithDescription.getShortFailureText();
-    final String expected = "This assert must fail (line 14)";
-    Assert.assertEquals(expected, actual);
-  }
-  
-  @Test
-  public void shortFailureTextWithoutDescription() {
-    final String actual = this.exceptionWithoutDescription.getShortFailureText();
-    final String expected = "Line 42";
-    Assert.assertEquals(expected, actual);
-  }
+    @Test
+    public void failedExpectationCallerLine() {
+        final Integer actual = getExpectationWithDescription().getCallerLine();
+        final Integer expected = Integer.valueOf(14);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortFailureTextWithDescription() {
+        final String actual = getExpectationWithDescription().getShortFailureText();
+        final String expected = "This assert must fail (line 14)";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortFailureTextWithoutDescription() {
+        final String actual = getExpectationWithoutDescription().getShortFailureText();
+        final String expected = "Line 42";
+        Assert.assertEquals(expected, actual);
+    }
 }
