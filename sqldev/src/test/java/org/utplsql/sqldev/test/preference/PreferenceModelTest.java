4642b021273dc8f48aaaa1474cb972836b150c4e
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-29 09:20:48 +0200

    add PreferenceModelTest.java generated by Xtend 2.20.0

--- sqldev/src/test/java/org/utplsql/sqldev/test/preference/PreferenceModelTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/preference/PreferenceModelTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,47 +13,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.test.preference
+package org.utplsql.sqldev.test.preference;
 
-import org.junit.Assert
-import org.junit.Test
-import org.utplsql.sqldev.model.preference.PreferenceModel
+import org.junit.Assert;
+import org.junit.Test;
+import org.utplsql.sqldev.model.preference.PreferenceModel;
 
-class PreferenceModelTest {
-
-	@Test
-	def defaultValues() {
-		val PreferenceModel model = PreferenceModel.getInstance(null)
-		Assert.assertTrue(model.useRealtimeReporter)
-		Assert.assertTrue(model.unsharedWorksheet)
-		Assert.assertFalse(model.resetPackage)
-		Assert.assertFalse(model.clearScreen)
-		Assert.assertTrue(model.autoExecute)
-		Assert.assertFalse(model.checkRunUtplsqlTest)
-		Assert.assertFalse(model.useSmartTimes)
-		Assert.assertEquals(model.numberOfRunsInHistory, 10)
-		Assert.assertFalse(model.showDisabledCounter)
-		Assert.assertFalse(model.showWarningsCounter)
-		Assert.assertFalse(model.showInfoCounter)
-		Assert.assertFalse(model.showWarningIndicator)
-		Assert.assertFalse(model.showInfoIndicator)
-		Assert.assertTrue(model.showSuccessfulTests)
-		Assert.assertTrue(model.showDisabledTests)
-		Assert.assertFalse(model.isShowTestDescription)
-		Assert.assertTrue(model.syncDetailTab)
-		Assert.assertEquals("test_", model.testPackagePrefix)
-		Assert.assertEquals("", model.testPackageSuffix)
-		Assert.assertEquals("", model.testUnitPrefix)
-		Assert.assertEquals("", model.testUnitSuffix)
-		Assert.assertEquals(1, model.numberOfTestsPerUnit)
-		Assert.assertFalse(model.checkGenerateUtplsqlTest)
-		Assert.assertTrue(model.generateComments)
-		Assert.assertFalse(model.disableTests)
-		Assert.assertEquals("alltests", model.suitePath)
-		Assert.assertEquals(3, model.indentSpaces)
-		Assert.assertTrue(model.generateFiles)
-		Assert.assertEquals(PreferenceModel.DEFAULT_OUTPUT_DIRECTORY, model.outputDirectory)
-		Assert.assertEquals(false, model.deleteExistingFiles)
-		Assert.assertEquals("utPLSQL", model.rootFolderInOddgenView)
-	}
+@SuppressWarnings("all")
+public class PreferenceModelTest {
+  @Test
+  public void defaultValues() {
+    final PreferenceModel model = PreferenceModel.getInstance(null);
+    Assert.assertTrue(model.isUseRealtimeReporter());
+    Assert.assertTrue(model.isUnsharedWorksheet());
+    Assert.assertFalse(model.isResetPackage());
+    Assert.assertFalse(model.isClearScreen());
+    Assert.assertTrue(model.isAutoExecute());
+    Assert.assertFalse(model.isCheckRunUtplsqlTest());
+    Assert.assertFalse(model.isUseSmartTimes());
+    Assert.assertEquals(model.getNumberOfRunsInHistory(), 10);
+    Assert.assertFalse(model.isShowDisabledCounter());
+    Assert.assertFalse(model.isShowWarningsCounter());
+    Assert.assertFalse(model.isShowInfoCounter());
+    Assert.assertFalse(model.isShowWarningIndicator());
+    Assert.assertFalse(model.isShowInfoIndicator());
+    Assert.assertTrue(model.isShowSuccessfulTests());
+    Assert.assertTrue(model.isShowDisabledTests());
+    Assert.assertFalse(model.isShowTestDescription());
+    Assert.assertTrue(model.isSyncDetailTab());
+    Assert.assertEquals("test_", model.getTestPackagePrefix());
+    Assert.assertEquals("", model.getTestPackageSuffix());
+    Assert.assertEquals("", model.getTestUnitPrefix());
+    Assert.assertEquals("", model.getTestUnitSuffix());
+    Assert.assertEquals(1, model.getNumberOfTestsPerUnit());
+    Assert.assertFalse(model.isCheckGenerateUtplsqlTest());
+    Assert.assertTrue(model.isGenerateComments());
+    Assert.assertFalse(model.isDisableTests());
+    Assert.assertEquals("alltests", model.getSuitePath());
+    Assert.assertEquals(3, model.getIndentSpaces());
+    Assert.assertTrue(model.isGenerateFiles());
+    Assert.assertEquals(PreferenceModel.DEFAULT_OUTPUT_DIRECTORY, model.getOutputDirectory());
+    Assert.assertEquals(Boolean.valueOf(false), Boolean.valueOf(model.isDeleteExistingFiles()));
+    Assert.assertEquals("utPLSQL", model.getRootFolderInOddgenView());
+  }
 }
