cf83c214abdc9a28bf0fd1d01614b9bf4b2e9e48
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-29 09:24:46 +0200

    add PreferencePanelTest.java generated by Xtend 2.20.0

--- sqldev/src/test/java/org/utplsql/sqldev/test/preference/PreferencePanelTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/preference/PreferencePanelTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,33 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.utplsql.sqldev.test.preference
+package org.utplsql.sqldev.test.preference;
 
-import java.awt.Dimension
-import java.awt.Toolkit
-import javax.swing.JFrame
-import javax.swing.SwingUtilities
-import org.junit.Test
-import org.utplsql.sqldev.ui.preference.PreferencePanel
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.junit.Test;
+import org.utplsql.sqldev.ui.preference.PreferencePanel;
 
-class PreferencePanelTest {
-	
-	@Test
-	def void layout() {
-		val frame = new JFrame("Preference Panel")
-		SwingUtilities.invokeLater(new Runnable() {
-			override run() {
-				val panel = new PreferencePanel
-				frame.add(panel)
-				frame.preferredSize = new Dimension(600, 400)
-				frame.pack
-				val dim = Toolkit.getDefaultToolkit().getScreenSize();
-				frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
-				frame.setVisible(true)
-			}
-		});
-		Thread.sleep(4 * 1000)
-		frame.dispose		
-	}
-	
+@SuppressWarnings("all")
+public class PreferencePanelTest {
+  @Test
+  public void layout() {
+    try {
+      final JFrame frame = new JFrame("Preference Panel");
+      SwingUtilities.invokeLater(new Runnable() {
+        @Override
+        public void run() {
+          final PreferencePanel panel = new PreferencePanel();
+          frame.add(panel);
+          Dimension _dimension = new Dimension(600, 400);
+          frame.setPreferredSize(_dimension);
+          frame.pack();
+          final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+          frame.setLocation(((dim.width / 2) - (frame.getSize().width / 2)), ((dim.height / 2) - (frame.getSize().height / 2)));
+          frame.setVisible(true);
+        }
+      });
+      Thread.sleep((4 * 1000));
+      frame.dispose();
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
 }
