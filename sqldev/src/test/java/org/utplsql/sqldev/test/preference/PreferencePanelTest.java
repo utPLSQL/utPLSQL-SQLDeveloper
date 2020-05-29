82e630ebfcb1f825b7fb23d3c1a386ab297ede9d
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-29 09:32:23 +0200

    convert PreferencePanelTest to Java, removing Xtend dependencies

--- sqldev/src/test/java/org/utplsql/sqldev/test/preference/PreferencePanelTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/preference/PreferencePanelTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,35 +17,31 @@ package org.utplsql.sqldev.test.preference;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import org.eclipse.xtext.xbase.lib.Exceptions;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.utplsql.sqldev.model.SystemTools;
 import org.utplsql.sqldev.ui.preference.PreferencePanel;
 
-@SuppressWarnings("all")
 public class PreferencePanelTest {
-  @Test
-  public void layout() {
-    try {
-      final JFrame frame = new JFrame("Preference Panel");
-      SwingUtilities.invokeLater(new Runnable() {
-        @Override
-        public void run() {
-          final PreferencePanel panel = new PreferencePanel();
-          frame.add(panel);
-          Dimension _dimension = new Dimension(600, 400);
-          frame.setPreferredSize(_dimension);
-          frame.pack();
-          final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-          frame.setLocation(((dim.width / 2) - (frame.getSize().width / 2)), ((dim.height / 2) - (frame.getSize().height / 2)));
-          frame.setVisible(true);
-        }
-      });
-      Thread.sleep((4 * 1000));
-      frame.dispose();
-    } catch (Throwable _e) {
-      throw Exceptions.sneakyThrow(_e);
+
+    @Test
+    public void layout() {
+        final JFrame frame = new JFrame("Preference Panel");
+        SwingUtilities.invokeLater(() -> {
+            final PreferencePanel panel = new PreferencePanel();
+            frame.add(panel);
+            frame.setPreferredSize(new Dimension(600, 400));
+            frame.pack();
+            final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
+            frame.setVisible(true);
+        });
+        SystemTools.sleep((4 * 1000));
+        Assert.assertNotNull(frame);
+        frame.dispose();
     }
-  }
 }
