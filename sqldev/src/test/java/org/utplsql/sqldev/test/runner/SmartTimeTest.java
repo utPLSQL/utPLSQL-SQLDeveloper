6a4dc52f8489aef2e240aaaeebd4c25c9477d2d9
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-29 09:59:21 +0200

    convert SmartTimeTest to Java, removing Xtend dependencies

--- sqldev/src/test/java/org/utplsql/sqldev/test/runner/SmartTimeTest.java
+++ sqldev/src/test/java/org/utplsql/sqldev/test/runner/SmartTimeTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2019 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,125 +19,129 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.utplsql.sqldev.ui.runner.SmartTime;
 
-@SuppressWarnings("all")
 public class SmartTimeTest {
-  @Test
-  public void null_default() {
-    final String effective = new SmartTime(null, false).toString();
-    Assert.assertEquals(null, effective);
-  }
-  
-  @Test
-  public void null_smart() {
-    final String effective = new SmartTime(null, true).toString();
-    Assert.assertEquals(null, effective);
-  }
-  
-  @Test
-  public void ms_0_default() {
-    final String effective = new SmartTime(Double.valueOf(0.0), false).toString();
-    Assert.assertEquals("0.000", effective);
-  }
-  
-  @Test
-  public void ms_0_smart() {
-    final String effective = new SmartTime(Double.valueOf(0.0), true).toString();
-    Assert.assertEquals("0 ms", effective);
-  }
-  
-  @Test
-  public void ms_999_default() {
-    final String effective = new SmartTime(Double.valueOf(0.999), false).toString();
-    Assert.assertEquals("0.999", effective);
-  }
-  
-  @Test
-  public void ms_999_smart() {
-    final String effective = new SmartTime(Double.valueOf(0.999), true).toString();
-    Assert.assertEquals("999 ms", effective);
-  }
-  
-  @Test
-  public void s_1_default() {
-    final String effective = new SmartTime(Double.valueOf(1.0), false).toString();
-    Assert.assertEquals("1.000", effective);
-  }
-  
-  @Test
-  public void s_1_smart() {
-    final String effective = new SmartTime(Double.valueOf(1.0), true).toString();
-    Assert.assertEquals("1.000 s", effective);
-  }
-  
-  @Test
-  public void s_59_default() {
-    final String effective = new SmartTime(Double.valueOf(59.999), false).toString();
-    Assert.assertEquals("59.999", effective);
-  }
-  
-  @Test
-  public void s_59_smart() {
-    final String effective = new SmartTime(Double.valueOf(59.999), true).toString();
-    Assert.assertEquals("59.999 s", effective);
-  }
-  
-  @Test
-  public void min_1_default() {
-    final String effective = new SmartTime(Double.valueOf(60.0), false).toString();
-    Assert.assertEquals("60.000", effective);
-  }
-  
-  @Test
-  public void min_1_smart() {
-    final String effective = new SmartTime(Double.valueOf(60.0), true).toString();
-    Assert.assertEquals("1.00 min", effective);
-  }
-  
-  @Test
-  public void min_59_default() {
-    final String effective = new SmartTime(Double.valueOf(3599.999), false).toString();
-    Assert.assertEquals("3,599.999", effective);
-  }
-  
-  @Test
-  public void min_59_smart_and_rounded() {
-    final String effective = new SmartTime(Double.valueOf(3599.999), true).toString();
-    Assert.assertEquals("60.00 min", effective);
-  }
-  
-  @Test
-  public void h_1_default() {
-    final String effective = new SmartTime(Double.valueOf(3600.0), false).toString();
-    Assert.assertEquals("3,600.000", effective);
-  }
-  
-  @Test
-  public void h_1_smart() {
-    final String effective = new SmartTime(Double.valueOf(3600.0), true).toString();
-    Assert.assertEquals("1.00 h", effective);
-  }
-  
-  @Test
-  public void h_max_default() {
-    final String effective = new SmartTime(Double.valueOf(99999.999), false).toString();
-    Assert.assertEquals("99,999.999", effective);
-  }
-  
-  @Test
-  public void h_max_smart() {
-    final String effective = new SmartTime(Double.valueOf(99999.999), true).toString();
-    Assert.assertEquals("27.78 h", effective);
-  }
-  
-  @Test
-  public void h_higher_than_max_default() {
-    final String effective = new SmartTime(Double.valueOf(100000000.0), false).toString();
-    Assert.assertEquals("100,000,000.000", effective);
-  }
-  
-  @Test
-  public void h_higher_than_max_smart() {
-    final String effective = new SmartTime(Double.valueOf(100000000.0), true).toString();
-    Assert.assertEquals("27777.78 h", effective);
-  }
+
+    @Test
+    public void null_default() {
+        final String effective = new SmartTime(null, false).toString();
+        Assert.assertEquals(null, effective);
+    }
+
+    @Test
+    public void null_smart() {
+        final String effective = new SmartTime(null, true).toString();
+        Assert.assertEquals(null, effective);
+    }
+
+    @Test
+    public void ms_0_default() {
+        final String effective = new SmartTime(0.0, false).toString();
+        Assert.assertEquals("0.000", effective);
+    }
+
+    @Test
+    public void ms_0_smart() {
+        final String effective = new SmartTime(0.0, true).toString();
+        Assert.assertEquals("0 ms", effective);
+    }
+
+    @Test
+    public void ms_999_default() {
+        final String effective = new SmartTime(0.999, false).toString();
+        Assert.assertEquals("0.999", effective);
+    }
+
+    @Test
+    public void ms_999_smart() {
+        final String effective = new SmartTime(0.999, true).toString();
+        Assert.assertEquals("999 ms", effective);
+    }
+
+    @Test
+    public void s_1_default() {
+        final String effective = new SmartTime(1.0, false).toString();
+        Assert.assertEquals("1.000", effective);
+    }
+
+    @Test
+    public void s_1_smart() {
+        final String effective = new SmartTime(1.0, true).toString();
+        Assert.assertEquals("1.000 s", effective);
+    }
+
+    @Test
+    public void s_59_default() {
+        final String effective = new SmartTime(59.999, false).toString();
+        Assert.assertEquals("59.999", effective);
+    }
+
+    @Test
+    public void s_59_smart() {
+        final String effective = new SmartTime(59.999, true).toString();
+        Assert.assertEquals("59.999 s", effective);
+    }
+
+    @Test
+    public void min_1_default() {
+        final String effective = new SmartTime(60.0, false).toString();
+        Assert.assertEquals("60.000", effective);
+    }
+
+    @Test
+    public void min_1_smart() {
+        final String effective = new SmartTime(60.0, true).toString();
+        Assert.assertEquals("1.00 min", effective);
+    }
+
+    @Test
+    public void min_59_default() {
+        final String effective = new SmartTime(3599.999, false).toString();
+        Assert.assertEquals("3,599.999", effective);
+    }
+
+    @Test
+    public void min_59_smart_and_rounded() {
+        final String effective = new SmartTime(3599.999, true).toString();
+        Assert.assertEquals("60.00 min", effective);
+    }
+
+    @Test
+    public void h_1_default() {
+        final String effective = new SmartTime(3600.0, false).toString();
+        Assert.assertEquals("3,600.000", effective);
+    }
+
+    @Test
+    public void h_1_smart() {
+        final String effective = new SmartTime(3600.0, true).toString();
+        Assert.assertEquals("1.00 h", effective);
+    }
+
+    @Test
+    public void h_max_default() {
+        final String effective = new SmartTime(99999.999, false).toString();
+        Assert.assertEquals("99,999.999", effective);
+    }
+
+    @Test
+    public void h_max_smart() {
+        final String effective = new SmartTime(99999.999, true).toString();
+        Assert.assertEquals("27.78 h", effective);
+    }
+
+    @Test
+    public void h_higher_than_max_default() {
+        // larger than format mask
+        // grouping separator applied, even if not specified in format mask
+        final String effective = new SmartTime(100000000.0, false).toString();
+        Assert.assertEquals("100,000,000.000", effective);
+    }
+
+    @Test
+    public void h_higher_than_max_smart() {
+        // larger than format mask
+        // no grouping separator applied (that's ok)
+        final String effective = new SmartTime(100000000.0, true).toString();
+        Assert.assertEquals("27777.78 h", effective);
+    }
 }
