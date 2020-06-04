bcf544ef149ff30d44c44b235cbe46af6ed21089
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-06-04 12:52:02 +0200

    remove unnecessary whitespace

--- sqldev/src/main/java/org/utplsql/sqldev/dal/RealtimeReporterDao.java
+++ sqldev/src/main/java/org/utplsql/sqldev/dal/RealtimeReporterDao.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2018 Philipp Salvisberg <philipp.salvisberg@trivadis.com>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -211,8 +211,8 @@ public class RealtimeReporterDao {
             rs.close();
             return sb1.toString();
         });
-    }    
-    
+    }
+
     private RealtimeReporterEvent convert(final String itemType, final String text) {
         logger.fine(() -> "\n---- " + itemType + " ----\n" + text);
         try {
